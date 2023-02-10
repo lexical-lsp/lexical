1af916cd1c56cf7c0814f57903aae7a883a842ca
Steve Cohen <scohen@scohen.org>
2023-02-10 09:18:36 -0800

    Changed name of log

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,7 @@ cond do
       backends: [{LoggerFileBackend, :general_log}]
 
     config :logger, :general_log,
-      path: "/Users/steve/Projects/lexical/lsp.log",
+      path: "/Users/steve/Projects/lexical/lexical.log",
       handle_sasl_reports: true,
       handle_otp_reports: true,
       level: :debug
