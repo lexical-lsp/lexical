a64933948fd1edcf71660d1dd24214ab00d82453
scottming <therealscottming@gmail.com>
2023-03-12 12:47:00 +0800

    Add a simple GitHub actions for running mix test

--- apps/remote_control/.formatter.exs
+++ apps/remote_control/.formatter.exs
@@ -1,4 +1,12 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs:
+    Enum.flat_map(
+      ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+      &Path.wildcard(&1, match_dot: true)
+    ) --
+      [
+        "test/fixtures/compilation_errors/lib/compilation_errors.ex",
+        "test/fixtures/parse_errors/lib/parse_errors.ex"
+      ]
 ]
