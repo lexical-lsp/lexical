3bd8a19b13ad851ea5bf00b79d87cfcb94dc5606
scottming <therealscottming@gmail.com>
2023-03-15 10:24:22 +0800

    Add two property tests

--- apps/common/test/lexical/math_test.exs
+++ apps/common/test/lexical/math_test.exs
@@ -2,6 +2,7 @@ defmodule Lexical.MathTest do
   alias Lexical.Math
 
   use ExUnit.Case, async: true
+  use ExUnitProperties
 
   describe "clamp/3" do
     test "returns low when value is less than low" do
@@ -14,6 +15,64 @@ defmodule Lexical.MathTest do
 
     test "returns high when value is greater than high" do
       assert Math.clamp(6, 0, 5) == 5
+    end
+  end
+
+  property "clamp works with all integers by simpler property" do
+    check all(
+            value <- integer(),
+            low = value - 1,
+            high = value + 1
+          ) do
+      assert Math.clamp(value, low, high) == value
+    end
+
+    check all(
+            low <- integer(),
+            value = low - 1,
+            high = value + 1
+          ) do
+      assert Math.clamp(value, low, high) == low
+    end
+
+    check all(
+            high <- integer(),
+            low = high - 1,
+            value = low - 1
+          ) do
+      assert Math.clamp(value, low, high) == low
+    end
+  end
+
+  property "clamp works with all positive_integers" do
+    check all(
+            low <- positive_integer(),
+            value <- integer(0..low),
+            high_range = (low + 1)..(low + 1_000_000),
+            high <-
+              integer(high_range)
+          ) do
+      assert Math.clamp(value, low, high) == low
+    end
+
+    check all(
+            value <- positive_integer(),
+            low <- integer(0..value),
+            high_range = (value + 1)..(value + 1_000_000),
+            high <-
+              integer(high_range)
+          ) do
+      assert Math.clamp(value, low, high) == value
+    end
+
+    check all(
+            high <- positive_integer(),
+            low <- integer(0..high),
+            value_range = (high + 1)..(high + 1_000_000),
+            value <-
+              integer(value_range)
+          ) do
+      assert Math.clamp(value, low, high) == high
     end
   end
 end
