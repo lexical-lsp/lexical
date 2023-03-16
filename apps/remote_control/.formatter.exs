4064ea910cef7c2a09506604111694d8c04f4c1f
scottming <therealscottming@gmail.com>
2023-03-16 08:41:58 +0800

    Apply suggestions of makeing the formatter.exs right

--- apps/remote_control/.formatter.exs
+++ apps/remote_control/.formatter.exs
@@ -1,12 +1,18 @@
 # Used by "mix format"
+current_directory = Path.dirname(__ENV__.file)
+
+impossible_to_format = [
+  Path.join(["test", "fixtures", "compilation_errors", "lib", "compilation_errors.ex"]),
+  Path.join(["test", "fixtures", "parse_errors", "lib", "parse_errors.ex"])
+]
+
 [
   inputs:
     Enum.flat_map(
-      ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-      &Path.wildcard(&1, match_dot: true)
-    ) --
       [
-        "test/fixtures/compilation_errors/lib/compilation_errors.ex",
-        "test/fixtures/parse_errors/lib/parse_errors.ex"
-      ]
+        Path.join(current_directory, "*.exs"),
+        Path.join(current_directory, "{lib,test}/**/*.{ex,exs}")
+      ],
+      &Path.wildcard(&1, match_dot: true)
+    ) -- impossible_to_format
 ]
