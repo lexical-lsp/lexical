79b225c0c16cf732b62b1604a3d4fb352253a11f
scottming <therealscottming@gmail.com>
2023-03-19 10:25:16 +0800

    Move the error_test.exs to the right place

--- apps/remote_control/test/lexical/remote_control/build/error_test.exs
+++ apps/remote_control/test/lexical/remote_control/build/error_test.exs
@@ -3,6 +3,14 @@ defmodule Lexical.RemoteControl.Build.ErrorTest do
 
   use ExUnit.Case, async: true
 
+  def to_quoted(source) do
+    Code.string_to_quoted(source)
+  end
+
+  def parse_error({:error, {a, b, c}}) do
+    Error.parse_error_to_diagnostics(a, b, c)
+  end
+
   describe "normalize_diagnostic/1" do
     test "normalizes the message when its a iodata" do
       diagnostic = %Mix.Task.Compiler.Diagnostic{
@@ -22,6 +30,46 @@ defmodule Lexical.RemoteControl.Build.ErrorTest do
 
       assert normalized.message ==
                ":slave.stop/1 is deprecated. It will be removed in OTP 27. Use the 'peer' module instead"
+    end
+  end
+
+  describe "handling parse errors" do
+    test "handles token missing errors" do
+      assert [diagnostic] =
+               ~s[%{foo: 3]
+               |> to_quoted()
+               |> parse_error()
+
+      assert diagnostic.message =~ ~s[missing terminator: } (for "{" starting at line 1)]
+    end
+
+    test "returns both the error and the detail when provided" do
+      errors =
+        ~S[
+        def handle_info(file_diagnostics(uri: uri, diagnostics: diagnostics), %State{} = state) do
+        state = State.clear(state, uri)
+        state = Enum.reduce(diagnostics, state, fn diagnostic, state ->
+          case State.add(diagnostic, state, uri) do
+            {:ok, new_state} ->
+              new_state
+            {:error, reason} ->
+              Logger.error("Could not add diagnostic #{inspect(diagnostic)} because #{inspect(error)}")
+              state
+          end
+        end
+
+          publish_diagnostics(state)
+        end
+        ]
+        |> to_quoted()
+        |> parse_error()
+
+      assert [error, detail] = errors
+      assert error.message =~ "unexpected reserved word: end"
+      assert error.position == {15, 9}
+
+      assert detail.message =~ ~S[The "(" here is missing terminator ")"]
+      assert detail.position == 4
     end
   end
 end
