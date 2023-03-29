4441694a42de43ac192491846526bf53ba21e1a0
Steve Cohen <scohen@scohen.org>
2023-03-29 08:55:21 -0700

    Additional improvements
    1. Applied namespacing to protocols and implementations
    2. Applied transforms to the app's consolidation directory
    3. Added percentage complete indicator to task

--- apps/common/lib/mix/tasks/namespace.ex
+++ apps/common/lib/mix/tasks/namespace.ex
@@ -11,17 +11,41 @@ defmodule Mix.Tasks.Namespace do
 
     app_globs = "{" <> Enum.join(app_names, ",") <> "}"
 
-    beam_files =
-      [Mix.Project.build_path(), "lib", app_globs, "**", "ebin", "Elixir.Lexical.*.beam"]
+    module_beams =
+      [Mix.Project.build_path(), "lib", app_globs, "**", "ebin", "Elixir.*Lexical*.beam"]
       |> Path.join()
       |> Path.wildcard()
 
-    Enum.each(beam_files, fn beam_file ->
+    consolidated_beams =
+      [Mix.Project.consolidation_path(), "**", "Elixir.*Lexical*.beam"]
+      |> Path.join()
+      |> Path.wildcard()
+
+    beam_files = module_beams ++ consolidated_beams
+
+    file_count = length(beam_files)
+
+    beam_files
+    |> Enum.with_index()
+    |> Enum.each(fn {beam_file, index} ->
       Transform.transform(beam_file)
-      IO.write(".")
+
+      IO.write("\r")
+      percent_complete = format_percent(index, file_count)
+
+      IO.write("Applying namespace: #{percent_complete} complete")
     end)
 
-    Mix.Shell.IO.info("\nRewrite Complete. Generating apps")
+    Mix.Shell.IO.info("\nNamspace applied. Generating apps")
     Mix.Task.rerun("compile.app")
+  end
+
+  defp format_percent(current, max) do
+    int_val =
+      (current / max * 100)
+      |> round()
+      |> Integer.to_string()
+
+    String.pad_leading("#{int_val}%", 4)
   end
 end
