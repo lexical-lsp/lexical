99d2cec8f708d2e0faee1db76061fa7a236830e1
Sleepful <josepablov@gmail.com>
2023-04-05 02:59:04 -0600

    Improve namespace for .app files & its references.

--- apps/proto/lib/mix/tasks/namespace/release.ex
+++ apps/proto/lib/mix/tasks/namespace/release.ex
@@ -3,44 +3,54 @@ defmodule Mix.Tasks.Namespace.Release do
   `remote_control.app` must be namespaced too
   """
   use Mix.Task
-  @release_files ~w(start.script start_clean.script lexical.rel)
-  @apps_to_rewrite ~w(remote_control)
+  @release_files ~w(start.script lexical.rel)
+  @apps_to_rewrite ~w(remote_control common common_protocol server protocol proto)
 
-  def run([release_path, release_version_path]) do
-    Enum.map(@apps_to_rewrite, &update_app(release_path, release_version_path, &1))
+  def run(_) do
+    release = Mix.Release.from_config!(:lexical, Mix.Project.config(), [])
+    Enum.map(@apps_to_rewrite, &update_app(release.path, release.version_path, &1))
+    # namespace .app filenames because the filename is used as identifier by BEAM
+    Enum.map(@apps_to_rewrite, &namespace_app_file(release.path, &1))
+    Mix.Shell.IO.info("\nApplied namespace to release app.")
   end
 
-  defp update_app(release_path, release_version_path, app_name) do
-    app_file_name = "#{app_name}.app"
-
+  defp path(:ebin, release_path, app_name) do
     [ebin_path] =
       [release_path, "lib", "#{app_name}-*", "ebin"]
       |> Path.join()
       |> Path.wildcard()
 
-    app_file_path = Path.join([ebin_path, app_file_name])
+    ebin_path
+  end
+
+  defp path(:app_file, release_path, app_name) do
+    Path.join([path(:ebin, release_path, app_name), "#{app_name}.app"])
+  end
+
+  defp update_app(release_path, release_version_path, app_name) do
+    # Rename references in release scripts
     release_file_paths = Enum.map(@release_files, &Path.join([release_version_path, &1]))
-    paths = [app_file_path | release_file_paths]
+    # Rename references in the dependencies of app files
+    apps_file_paths = Enum.map(@apps_to_rewrite, &path(:app_file, release_path, &1))
+    paths = apps_file_paths ++ release_file_paths
 
-    Enum.each(paths, &update_file_contents(&1, "remote_control"))
-
-    # rename .app file because the filename is used as identifier by BEAM
-    namespace_app_file(app_file_path)
-
-    Mix.Shell.IO.info("\nApplied namespace to release app.")
+    Enum.each(paths, &update_file_contents(&1, app_name))
   end
 
   defp update_file_contents(path, app_name) do
     contents = File.read!(path)
-    # avoid replacing directory path with negative lookbehind:
-    updated_contents = String.replace(contents, ~r/(?<!\/)#{app_name}/, "lx_#{app_name}")
+    # matches if preceding characters involves either of: , " [ { [:blank:]
+    # this way it doesn't match on substrings or directory names
+    updated_contents =
+      String.replace(contents, ~r/([,"\[{[:blank:]])#{app_name}/, "\\1lx_#{app_name}")
+
     File.write!(path, updated_contents)
   end
 
-  defp namespace_app_file(app_file_path) do
-    app_file_dir = Path.dirname(app_file_path)
-    app_file_name = Path.basename(app_file_path)
-    namespaced_app_file = Path.join([app_file_dir, "lx_" <> app_file_name])
+  defp namespace_app_file(release_path, app_name) do
+    ebin_path = path(:ebin, release_path, app_name)
+    app_file_path = path(:app_file, release_path, app_name)
+    namespaced_app_file = Path.join([ebin_path, "lx_" <> "#{app_name}.app"])
     :ok = File.rename(app_file_path, namespaced_app_file)
   end
 end
