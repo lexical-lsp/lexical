264c32aa1fee00168e96b852594a5632f0943367
scottming <therealscottming@gmail.com>
2023-04-09 15:17:38 +0800

    Add more tests for the definition

--- apps/remote_control/test/fixtures/navigations/lib/my_definition.ex
+++ apps/remote_control/test/fixtures/navigations/lib/my_definition.ex
@@ -1,8 +1,28 @@
 defmodule MyDefinition do
   @type result :: String.t()
 
+  defmacro __using__(_opts) do
+    quote do
+      import MyDefinition
+    end
+  end
+
   @spec greet(String.t()) :: result
   def greet(name) do
     "Hello, #{name}!"
+  end
+
+  defmacro print_hello do
+    quote do
+      IO.puts("Hello, world!")
+    end
+  end
+
+  def uses_greet() do
+    greet("world")
+  end
+
+  def uses_print_hello_macro() do
+    print_hello()
   end
 end
--- apps/remote_control/test/lexical/remote_control/code_intelligence/definition_test.exs
+++ apps/remote_control/test/lexical/remote_control/code_intelligence/definition_test.exs
@@ -27,41 +27,197 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
     file_path(project, Path.join("lib", name))
   end
 
-  defp open_uses_file(project) do
-    uses = ~q[
-      defmodule MyModule do
-        alias MyDefinition
-
-        def my_function() do
-          MyDefinition.greet()
-        end
-      end
-      ]
-
+  defp open_uses_file(project, content) do
     path = project_file_path(project, "my_module.ex")
     uri = SourceFile.Path.ensure_uri(path)
-
-    :ok = SourceFile.Store.open(uri, uses, 1)
+    :ok = SourceFile.Store.open(uri, content, 1)
     {:ok, uses_file} = SourceFile.Store.fetch(uri)
     uses_file
   end
 
   defp cursor_to_position(cursor, line, source_file) do
     {:ok, text} = SourceFile.fetch_text_at(source_file, line)
-    cursor = String.trim_trailing(cursor, "|")
+    [cursor | _] = String.split(cursor, "|", parts: 2)
     [before_cursor, _] = String.split(text, cursor, parts: 2)
     character = String.length(before_cursor) + String.length(cursor) + 1
     Position.new(line, character)
   end
 
-  describe "definition/2" do
+  describe "definition/2 when making remote call by alias" do
     setup [:with_navigation_project, :with_referenced_file]
 
-    test "find the definition of a remote function call", %{project: project, uri: referenced_uri} do
-      uses_file = open_uses_file(project)
-      position = cursor_to_position("MyDefinition.gree|", 5, uses_file)
+    setup do
+      uses_content = ~q[
+        defmodule MyModule do
+          alias MyDefinition
+          require MyDefinition
+
+          def my_function() do
+            MyDefinition.greet("World")
+          end
+
+          def uses_macro() do
+            MyDefinition.print_hello()
+          end
+        end
+        ]
+      %{uses_content: uses_content}
+    end
+
+    test "find the definition of a remote function call", ctx do
+      %{project: project, uri: referenced_uri, uses_content: uses_content} = ctx
+      uses_file = open_uses_file(project, uses_content)
+      position = cursor_to_position("MyDefinition.gree|", 6, uses_file)
 
       {:ok, {source_file, range}} = Definition.definition(uses_file, position)
+
+      assert annotate(source_file, range) == ~q[
+        defmodule MyDefinition do
+        ...
+
+          def greet(name) do
+        #     ^^^^^
+      ]
+
+      assert source_file.uri == referenced_uri
+    end
+
+    test "find the definition of the module", ctx do
+      %{project: project, uri: referenced_uri, uses_content: uses_content} = ctx
+      uses_file = open_uses_file(project, uses_content)
+      position = cursor_to_position("MyDef|inition", 6, uses_file)
+
+      {:ok, {source_file, range}} = Definition.definition(uses_file, position)
+
+      assert annotate(source_file, range) == ~q[
+        defmodule MyDefinition do
+        #         ^^^^^^^^^^^^
+      ]
+
+      assert source_file.uri == referenced_uri
+    end
+
+    test "find the macro definition", ctx do
+      %{project: project, uri: referenced_uri, uses_content: uses_content} = ctx
+      uses_file = open_uses_file(project, uses_content)
+      position = cursor_to_position("MyDefinition.print_hello|", 10, uses_file)
+
+      {:ok, {source_file, range}} = Definition.definition(uses_file, position)
+
+      assert annotate(source_file, range) == ~q[
+        defmodule MyDefinition do
+        ...
+
+          defmacro print_hello do
+        #          ^^^^^^^^^^^
+      ]
+
+      assert source_file.uri == referenced_uri
+    end
+  end
+
+  describe "definition/2 when making remote call by import" do
+    setup [:with_navigation_project, :with_referenced_file]
+
+    setup do
+      uses_content = ~q[
+        defmodule MyModule do
+          import MyDefinition
+
+          def my_function() do
+            greet("World")
+          end
+
+          def uses_macro() do
+            print_hello()
+          end
+        end
+        ]
+
+      %{uses_content: uses_content}
+    end
+
+    test "find the definition of a remote function call", ctx do
+      %{project: project, uri: referenced_uri, uses_content: uses_content} = ctx
+      uses_file = open_uses_file(project, uses_content)
+      position = cursor_to_position("greet|", 5, uses_file)
+
+      {:ok, {source_file, range}} = Definition.definition(uses_file, position)
+
+      assert annotate(source_file, range) == ~q[
+        defmodule MyDefinition do
+        ...
+
+          def greet(name) do
+        #     ^^^^^
+      ]
+
+      assert source_file.uri == referenced_uri
+    end
+
+    test "find the definition of a remote macro call", ctx do
+      %{project: project, uri: referenced_uri, uses_content: uses_content} = ctx
+      uses_file = open_uses_file(project, uses_content)
+      position = cursor_to_position("print_hello|", 9, uses_file)
+
+      {:ok, {source_file, range}} = Definition.definition(uses_file, position)
+
+      assert annotate(source_file, range) == ~q[
+        defmodule MyDefinition do
+        ...
+
+          defmacro print_hello do
+        #          ^^^^^^^^^^^
+      ]
+
+      assert source_file.uri == referenced_uri
+    end
+  end
+
+  describe "definition/2 when making remote call by use to import definition" do
+    setup [:with_navigation_project, :with_referenced_file]
+
+    setup do
+      uses_content = ~q[
+        defmodule MyModule do
+          use MyDefinition
+
+          def my_function() do
+            greet("World")
+          end
+        end
+        ]
+
+      %{uses_content: uses_content}
+    end
+
+    test "find the function definition", ctx do
+      %{project: project, uses_content: uses_content} = ctx
+      uses_file = open_uses_file(project, uses_content)
+      position = cursor_to_position("greet|", 5, uses_file)
+
+      assert {:ok, {source_file, range}} = Definition.definition(uses_file, position)
+
+      assert annotate(source_file, range) == ~q[
+        defmodule MyDefinition do
+        ...
+
+          def greet(name) do
+        #     ^^^^^
+      ]
+    end
+  end
+
+  describe "definition/2 when making local call" do
+    setup [:with_navigation_project, :with_referenced_file]
+
+    test "find the function definition", ctx do
+      %{uri: referenced_uri} = ctx
+
+      {:ok, uses_file} = SourceFile.Store.open_temporary(referenced_uri)
+      position = cursor_to_position("gree|", 22, uses_file)
+
+      assert {:ok, {source_file, range}} = Definition.definition(uses_file, position)
 
       assert annotate(source_file, range) == ~q[
         defmodule MyDefinition do
@@ -86,6 +242,10 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         String.duplicate(" ", range.start.character - 2) <>
         String.duplicate("^", range.end.character - range.start.character)
 
-    Enum.join([module_part, definition_line, annotation], "\n") <> "\n"
+    if range.start.line == 1 do
+      Enum.join([definition_line, annotation], "\n") <> "\n"
+    else
+      Enum.join([module_part, definition_line, annotation], "\n") <> "\n"
+    end
   end
 end
