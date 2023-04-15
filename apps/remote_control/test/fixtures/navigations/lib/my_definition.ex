2db39ea9a42de769d43184d8b1140c0c89a32c10
scottming <therealscottming@gmail.com>
2023-04-15 09:59:19 +0800

    Remove unused definition functions

--- apps/remote_control/test/fixtures/navigations/lib/my_definition.ex
+++ apps/remote_control/test/fixtures/navigations/lib/my_definition.ex
@@ -21,32 +21,4 @@ defmodule MyDefinition do
       IO.puts("Hello, world!")
     end
   end
-
-  def uses_greet() do
-    greet("world")
-  end
-
-  def uses_print_hello_macro() do
-    print_hello()
-  end
-
-  @b 2
-
-  def uses_variable_and_attr() do
-    a = 1
-
-    if a == 1 do
-      a
-    else
-      @b
-    end
-  end
-
-  def uses_elixir_std_module() do
-    String.to_integer("123")
-  end
-
-  def uses_erlang_module() do
-    :erlang.binary_to_atom("hello", :utf8)
-  end
 end
