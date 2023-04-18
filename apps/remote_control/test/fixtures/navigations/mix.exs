ed6798bda20de06c5a178952bf6fc1d7a32f0966
scottming <therealscottming@gmail.com>
2023-04-18 08:06:28 +0800

    Apply some code review

--- apps/remote_control/test/fixtures/navigations/mix.exs
+++ apps/remote_control/test/fixtures/navigations/mix.exs
@@ -20,9 +20,6 @@ defmodule Navigations.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
--- apps/remote_control/test/lexical/remote_control/code_intelligence/definition_test.exs
+++ apps/remote_control/test/lexical/remote_control/code_intelligence/definition_test.exs
@@ -11,22 +11,8 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
 
   use ExUnit.Case, async: false
 
-  defp with_navigation_project(_ctx) do
-    project = project(:navigations)
-    {:ok, _, _} = RemoteControl.start_link(project, self())
-
-    on_exit(fn ->
-      :ok = RemoteControl.stop(project)
-    end)
-
-    RemoteControl.Api.schedule_compile(project, true)
-    assert_receive project_compiled(), 5000
-
-    %{project: project}
-  end
-
-  defp with_referenced_file(%{project: project}, file \\ "my_definition.ex") do
-    path = file_path(project, Path.join("lib", file))
+  defp with_referenced_file(%{project: project}) do
+    path = file_path(project, Path.join("lib", "my_definition.ex"))
     %{uri: SourceFile.Path.ensure_uri(path)}
   end
 
@@ -44,7 +30,19 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
     end
   end
 
-  setup_all ~w(with_navigation_project)a
+  setup_all do
+    project = project(:navigations)
+    {:ok, _, _} = RemoteControl.start_link(project, self())
+
+    on_exit(fn ->
+      :ok = RemoteControl.stop(project)
+    end)
+
+    RemoteControl.Api.schedule_compile(project, true)
+    assert_receive project_compiled(), 5000
+
+    %{project: project}
+  end
 
   setup %{project: project} do
     uri = uses_file_uri(project)
@@ -59,7 +57,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
     setup [:with_referenced_file]
 
     test "find the definition of a remote function call", %{project: project, uri: referenced_uri} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           alias MyDefinition
 
@@ -69,12 +67,12 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         end
         ]
 
-      assert {:ok, ^referenced_uri, definition_line} = definition(project, uses_content)
+      assert {:ok, ^referenced_uri, definition_line} = definition(project, subject_module)
       assert definition_line == ~S[  def «greet»(name) do]
     end
 
     test "find the definition of the module", %{project: project, uri: referenced_uri} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           alias MyDefinition
 
@@ -84,12 +82,12 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         end
         ]
 
-      assert {:ok, ^referenced_uri, definition_line} = definition(project, uses_content)
+      assert {:ok, ^referenced_uri, definition_line} = definition(project, subject_module)
       assert definition_line == ~S[defmodule «MyDefinition» do]
     end
 
     test "find the macro definition", %{project: project, uri: referenced_uri} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           alias MyDefinition
 
@@ -99,12 +97,12 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         end
         ]
 
-      assert {:ok, ^referenced_uri, definition_line} = definition(project, uses_content)
+      assert {:ok, ^referenced_uri, definition_line} = definition(project, subject_module)
       assert definition_line == ~S[  defmacro «print_hello» do]
     end
 
     test "it can't find the right arity function definition", %{project: project} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           alias MultiArity
 
@@ -117,7 +115,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
       # TODO: this is a limitation of elixir_sense
       # can be fixed when we have a tracer
       # when function is imported, it also has the issue
-      {:ok, referenced_uri, definition_line} = definition(project, uses_content)
+      {:ok, referenced_uri, definition_line} = definition(project, subject_module)
 
       assert definition_line == ~S[  def «sum»(a, b) do]
       assert referenced_uri =~ "navigations/lib/multi_arity.ex"
@@ -128,7 +126,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
     setup [:with_referenced_file]
 
     test "find the definition of a remote function call", %{project: project, uri: referenced_uri} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           import MyDefinition
 
@@ -138,13 +136,13 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         end
         ]
 
-      assert {:ok, ^referenced_uri, definition_line} = definition(project, uses_content)
+      assert {:ok, ^referenced_uri, definition_line} = definition(project, subject_module)
       assert definition_line == ~S[  def «greet»(name) do]
     end
 
     test "find the definition of a remote macro call",
          %{project: project, uri: referenced_uri} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           import MyDefinition
 
@@ -154,7 +152,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         end
         ]
 
-      assert {:ok, ^referenced_uri, definition_line} = definition(project, uses_content)
+      assert {:ok, ^referenced_uri, definition_line} = definition(project, subject_module)
       assert definition_line == ~S[  defmacro «print_hello» do]
     end
   end
@@ -163,7 +161,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
     setup [:with_referenced_file]
 
     test "find the function definition", %{project: project, uri: referenced_uri} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           use MyDefinition
 
@@ -172,14 +170,14 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
           end
         end
         ]
-      assert {:ok, ^referenced_uri, definition_line} = definition(project, uses_content)
+      assert {:ok, ^referenced_uri, definition_line} = definition(project, subject_module)
       assert definition_line == ~S[  def «greet»(name) do]
     end
 
     test "it can't find the correct definition when func defined in the quote block", %{
       project: project
     } do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesRemoteFunction do
           use MyDefinition
 
@@ -188,7 +186,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
           end
         end
         ]
-      {:ok, referenced_uri, definition_line} = definition(project, uses_content)
+      {:ok, referenced_uri, definition_line} = definition(project, subject_module)
 
       # credo:disable-for-next-line Credo.Check.Design.TagTODO
       # TODO: this is wrong, it should go to the definition in the quote block
@@ -200,10 +198,8 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
   end
 
   describe "definition/2 when making local call" do
-    setup [:with_referenced_file]
-
     test "find the function definition", %{project: project} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesOwnFunction do
           def greet do
           end
@@ -214,14 +210,14 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
         end
       ]
 
-      {:ok, referenced_uri, definition_line} = definition(project, uses_content)
+      {:ok, referenced_uri, definition_line} = definition(project, subject_module)
 
       assert definition_line == ~S[  def «greet» do]
       assert referenced_uri =~ "navigations/lib/my_module.ex"
     end
 
     test "find the attribute", %{project: project} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesAttribute do
           @b 2
 
@@ -230,14 +226,14 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
           end
         end
         ]
-      {:ok, referenced_uri, definition_line} = definition(project, uses_content)
+      {:ok, referenced_uri, definition_line} = definition(project, subject_module)
 
       assert definition_line =~ ~S[«@b» 2]
       assert referenced_uri =~ "navigations/lib/my_module.ex"
     end
 
     test "find the variable", %{project: project} do
-      uses_content = ~q[
+      subject_module = ~q[
         defmodule UsesVariable do
           def use_variable do
             a = 1
@@ -248,43 +244,43 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
           end
         end
       ]
-      {:ok, referenced_uri, definition_line} = definition(project, uses_content)
+      {:ok, referenced_uri, definition_line} = definition(project, subject_module)
 
       assert definition_line =~ ~S[«a» = 1]
       assert referenced_uri =~ "navigations/lib/my_module.ex"
     end
 
-    test "can't find the definition when call a Elixir std module function",
+    test "can't find the definition when calling a Elixir std module function",
          %{project: project} do
-      uses_content = ~q[
+      subject_module = ~q[
         String.to_integer|("1")
       ]
 
       # credo:disable-for-next-line Credo.Check.Design.TagTODO
       # TODO: this should be fixed when we have a call tracer
-      {:ok, nil} = definition(project, uses_content)
+      {:ok, nil} = definition(project, subject_module)
     end
 
-    test "find the definition when call a erlang module", %{project: project} do
-      uses_content = ~q[
+    test "find the definition when calling a erlang module", %{project: project} do
+      subject_module = ~q[
         :erlang.binary_to_atom|("1")
       ]
-      {:ok, uri, definition_line} = definition(project, uses_content)
+      {:ok, uri, definition_line} = definition(project, subject_module)
 
       assert uri =~ "/src/erlang.erl"
       assert definition_line =~ ~S[«binary_to_atom»(Binary)]
     end
   end
 
-  defp caller_position(uses_content) do
-    {line, character} = cursor_position(uses_content)
+  defp caller_position(subject_module) do
+    {line, character} = cursor_position(subject_module)
     Position.new(line, character)
   end
 
-  defp definition(project, uses_content) do
-    position = caller_position(uses_content)
+  defp definition(project, subject_module) do
+    position = caller_position(subject_module)
 
-    with {:ok, uses_file} <- open_uses_file(project, strip_cursor(uses_content)),
+    with {:ok, uses_file} <- open_uses_file(project, strip_cursor(subject_module)),
          {:ok, {source_file, range}} <-
            RemoteControl.Api.definition(project, uses_file, position),
          {:ok, definition_line} <- definition_line(source_file, range) do
@@ -297,7 +293,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
       graphemes = String.graphemes(line_text)
       {text_before_range, range_text, text_after_range} = extract_range(graphemes, range)
       marked_text = mark_range(range_text)
-      {:ok, IO.iodata_to_binary(text_before_range ++ marked_text ++ text_after_range)}
+      {:ok, IO.iodata_to_binary([text_before_range, marked_text, text_after_range])}
     end
   end
 
@@ -310,9 +306,10 @@ defmodule Lexical.RemoteControl.CodeIntelligence.DefinitionTest do
     {text_before_range, range_text, text_after_range}
   end
 
+  @range_start_marker "«"
+  @range_end_marker "»"
+
   defp mark_range(range_text) do
-    start_symbol = ~s[«]
-    end_symbol = ~s[»]
-    [start_symbol | range_text ++ List.wrap(end_symbol)]
+    [@range_start_marker | range_text ++ List.wrap(@range_end_marker)]
   end
 end
