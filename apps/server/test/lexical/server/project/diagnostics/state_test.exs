3bc7c22a8f01a15a64a8935fab370b69815ca844
Scott Ming <TheRealScottMing@gmail.com>
2023-04-20 08:33:58 +0800

    Fix script file diagnostics flushing (#105)
    * Fix test file diagnostics flushing
    * Use script_file? instead of test_file?
    * Apply the suggestions
    * Apply other suggestions for tests

--- apps/server/test/lexical/server/project/diagnostics/state_test.exs
+++ apps/server/test/lexical/server/project/diagnostics/state_test.exs
@@ -22,8 +22,8 @@ defmodule Lexical.Project.Diagnostics.StateTest do
     Path.join([Project.root_path(project()), "lib", "project.ex"])
   end
 
-  def source_file(contents \\ "") do
-    file_uri = SourceFile.Path.to_uri(existing_file_path())
+  def source_file(contents, file_path \\ existing_file_path()) do
+    file_uri = SourceFile.Path.to_uri(file_path)
 
     with :ok <- SourceFile.Store.open(file_uri, contents, 0),
          {:ok, source_file} <- SourceFile.Store.fetch(file_uri) do
@@ -154,10 +154,21 @@ defmodule Lexical.Project.Diagnostics.StateTest do
       {:ok, state} =
         State.add(state, compiler_diagnostic(message: "The code is awful"), source_file.uri)
 
+      old_diagnostics = State.get(state, source_file.uri)
       state = State.clear_all_flushed(state)
-      diagnostics = State.get(state, source_file.uri)
+      assert ^old_diagnostics = State.get(state, source_file.uri)
+    end
 
-      assert [_] = diagnostics
+    test "it should not clear a script file even if it is clean", %{state: state} do
+      script_file_path = Path.join([Project.root_path(project()), "test", "*.exs"])
+      source_file = source_file("assert f() == 0", script_file_path)
+
+      {:ok, state} =
+        State.add(state, compiler_diagnostic(message: "undefined function f/0"), source_file.uri)
+
+      old_diagnostics = State.get(state, source_file.uri)
+      state = State.clear_all_flushed(state)
+      assert ^old_diagnostics = State.get(state, source_file.uri)
     end
 
     test "it should clear a file's diagnostics if it is just open", %{state: state} do
