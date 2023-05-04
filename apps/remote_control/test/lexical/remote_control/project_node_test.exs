4542a88ff67a68a6c6735b5a4f846fcf5e722b0b
scottming <therealscottming@gmail.com>
2023-05-04 08:05:51 +0800

    Add a half-link test

--- apps/remote_control/test/lexical/remote_control/project_node_test.exs
+++ apps/remote_control/test/lexical/remote_control/project_node_test.exs
@@ -1,27 +1,42 @@
 defmodule Lexical.RemoteControl.ProjectNodeTest do
-  alias Lexical.Project
   alias Lexical.RemoteControl
   alias Lexical.RemoteControl.ProjectNode
   alias Lexical.RemoteControl.ProjectNodeSupervisor
 
   import Lexical.Test.Fixtures
+  import Testing.EventualAssertions
+
   use ExUnit.Case, async: false
 
-  def with_project(project_name) do
-    project_name = to_string(project_name)
-    fixture_dir = Path.join(fixtures_path(), project_name)
-    project = Project.new("file://#{fixture_dir}")
-
-    {:ok, _} = start_supervised({ProjectNodeSupervisor, project})
-    {:ok, project}
+  setup do
+    project = project(:project)
+    start_supervised!({ProjectNodeSupervisor, project})
+    {:ok, %{project: project}}
   end
 
-  test "it should be able to stop a project node and won't restart" do
-    {:ok, project} = with_project(:project_metadata)
+  test "it should be able to stop a project node and won't restart", %{project: project} do
     {:ok, _node_name, _} = RemoteControl.start_link(project, self())
 
     assert ProjectNode.name(project) |> Process.whereis() |> Process.alive?()
-    assert :ok = RemoteControl.stop(project)
-    assert Process.whereis(ProjectNode.name(project)) == nil
+    assert :ok = ProjectNode.stop(project)
+    assert_eventually Process.whereis(ProjectNode.name(project)) == nil, 50
+  end
+
+  test "it should be stopped atomically when the linked process is dead", %{project: project} do
+    test_pid = self()
+
+    linked_node_process =
+      spawn(fn ->
+        {:ok, _node_name, _} = RemoteControl.start_link(project, test_pid)
+        send(test_pid, :started)
+      end)
+
+    assert_receive :started, 1500
+
+    node_process_name = ProjectNode.name(project)
+
+    assert node_process_name |> Process.whereis() |> Process.alive?()
+    Process.exit(linked_node_process, :kill)
+    assert_eventually Process.whereis(node_process_name) == nil, 50
   end
 end
