cb987181c162fe9b3da44f5c388fb664ecefeefa
Scott Ming <TheRealScottMing@gmail.com>
2023-05-22 09:29:25 +0800

    Delete enhancement.ex

--- apps/common/lib/lexical/enhancement.ex
+++
@@ -1,9 +0,0 @@
-defmodule Lexical.Enhancement do
-  @enforce_keys [:project, :type, :validate, :enhance, :source]
-
-  defstruct project: nil, uri: nil, type: nil, validate: nil, enhance: nil, source: nil
-
-  def new(fields) do
-    struct!(__MODULE__, fields)
-  end
-end
