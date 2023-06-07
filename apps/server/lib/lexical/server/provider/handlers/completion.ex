8d2df089f1a021949f039f6b7aecf6a5ce5e36fd
Łukasz Niemier <lukasz@niemier.pl>
2023-06-07 20:44:44 +0200

    fix: completion context can be null
    Co-Authored-By: Steve Cohen <scohen@scohen.org>

--- apps/server/lib/lexical/server/provider/handlers/completion.ex
+++ apps/server/lib/lexical/server/provider/handlers/completion.ex
@@ -1,6 +1,7 @@
 defmodule Lexical.Server.Provider.Handlers.Completion do
   alias Lexical.Protocol.Requests
   alias Lexical.Protocol.Responses
+  alias Lexical.Protocol.Types.Completion
   alias Lexical.Server.CodeIntelligence
   alias Lexical.Server.Provider.Env
 
@@ -12,7 +13,7 @@ defmodule Lexical.Server.Provider.Handlers.Completion do
         env.project,
         request.document,
         request.position,
-        request.context
+        request.context || Completion.Context.new(trigger_kind: :invoked)
       )
 
     response = Responses.Completion.new(request.id, completions)
