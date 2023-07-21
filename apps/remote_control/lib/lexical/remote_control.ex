941468299b9c165d8f305c39206f05c04c5f3f9a
Étienne Lévesque <contact@etiennel.dev>
2023-07-21 15:22:45 -0400

    fix: Add missing command to get rtx env

--- apps/remote_control/lib/lexical/remote_control.ex
+++ apps/remote_control/lib/lexical/remote_control.ex
@@ -170,7 +170,7 @@ defmodule Lexical.RemoteControl do
   end
 
   defp reset_env(:rtx, root_path) do
-    {env, _} = System.cmd("rtx", ~w(), cd: root_path)
+    {env, _} = System.cmd("rtx", ~w(env), cd: root_path)
 
     env
     |> String.trim()
