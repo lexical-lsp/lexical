1ee640f2d560e3ecda1f9a5981a1bc9c225de04c
Steve Cohen <scohen@scohen.org>
2023-08-01 08:24:08 -0700

    Made the feature actually check the feature

--- apps/common/lib/elixir/features.ex
+++ apps/common/lib/elixir/features.ex
@@ -1,6 +1,6 @@
 defmodule Elixir.Features do
   def with_diagnostics? do
-    Version.match?(System.version(), ">= 1.15.3")
+    function_exported?(Code, :with_diagnostics, 1)
   end
 
   def compile_wont_change_directory? do
