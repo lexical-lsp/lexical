d91188a5c75e9afcd9347cc9359b90b2e3c57cc4
Steve Cohen <scohen@scohen.org>
2023-08-16 15:08:34 -0700

    Moved version checks to boot, added checks for erlang and elixir versions

--- apps/server/lib/lexical/server/application.ex
+++ apps/server/lib/lexical/server/application.ex
@@ -5,7 +5,6 @@ defmodule Lexical.Server.Application do
 
   alias Lexical.Server.Provider
   alias Lexical.Server.Transport
-  alias Lexical.VM.Versions
 
   require Logger
 
@@ -13,8 +12,6 @@ defmodule Lexical.Server.Application do
 
   @impl true
   def start(_type, _args) do
-    check_version()
-
     children = [
       Lexical.Document.Store,
       Lexical.Server,
@@ -26,33 +23,5 @@ defmodule Lexical.Server.Application do
 
     opts = [strategy: :one_for_one, name: Lexical.Server.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp check_version do
-    unless Versions.compatible?() do
-      {:ok, compiled_versions} = Versions.compiled()
-
-      compiled_versions = Versions.to_versions(compiled_versions)
-      current_versions = Versions.current() |> Versions.to_versions()
-
-      compiled_erlang = compiled_versions.erlang
-      current_erlang = current_versions.erlang
-
-      message = """
-      Lexical failed its version check. This is a FATAL Error!
-      Lexical is running on Erlang #{current_erlang} and the compiled files were built on
-      Erlang #{compiled_erlang}.
-
-      If you wish to run Lexical under Erlang version #{current_erlang}, you must rebuild lexical
-      under an Erlang version that is <= #{current_erlang.major}.
-
-      Detected Lexical running on erlang #{current_erlang.major} and needs >= #{compiled_erlang.major}
-      """
-
-      Logger.emergency(message)
-      # Wait for the logs to flush
-      Process.sleep(500)
-      System.halt()
-    end
   end
 end
