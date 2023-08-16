adca350fd8bfff92b893265bdf8cb03c8ff0c09e
Steve Cohen <scohen@scohen.org>
2023-08-16 09:02:55 -0700

    Bumped versions to 0.3.0

--- apps/proto/mix.exs
+++ apps/proto/mix.exs
@@ -4,7 +4,7 @@ defmodule Proto.MixProject do
   def project do
     [
       app: :proto,
-      version: "0.1.0",
+      version: "0.3.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
