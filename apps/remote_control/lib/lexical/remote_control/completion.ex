ccb7e3f0c12c02fec7dffe970b1b78fb8e50712d
Zach Allaun <zach.allaun@gmail.com>
2023-08-21 09:13:22 -0400

    Delete outdated comment
    This was a comment explaining why position line and character were being
    incremented when they used to be zero-based. They are now one-based, so
    this comment is no longer needed.

--- apps/remote_control/lib/lexical/remote_control/completion.ex
+++ apps/remote_control/lib/lexical/remote_control/completion.ex
@@ -5,9 +5,6 @@ defmodule Lexical.RemoteControl.Completion do
   alias Lexical.RemoteControl.Completion.Candidate
 
   def elixir_sense_expand(doc_string, %Position{} = position) do
-    # Add one to both the line and character, because elixir sense
-    # has one-based lines, and the character needs to be after the context,
-    # rather than in between.
     line = position.line
     character = position.character
     hint = ElixirSense.Core.Source.prefix(doc_string, line, character)
