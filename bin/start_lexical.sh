8205a8a931b43894a2840fddb07f1765f47e489b
Steve Cohen <scohen@scohen.org>
2023-08-23 13:01:58 -0700

    0.3.0 Release (#337)
    * Bumped versions to 0.3.0
    
    * Added version compatibility check (#332)
    
    * Added version compatibility check
    
    Lexical now outputs the erlang and elixir versions of the VM that was
    used to create them. It now checks the build and packaging directories
    for these versioning files and emits an error message if the vm starts
    with beam files that won't work.
    
    * Detected version change on call to package
    
    Another belt to wear with the suspenders. When we build a package that
    overwrites another package, if the version has changed, we delete the
    old compiled code and the old package and start compilation from
    scratch. This definitely prevents old beam files from ending up in the
    new package.
    
    * Fixed dialyzer error
    
    halt/1 always raises which irritates dialyzer. Added an ignore to it.
    
    * Improved launcher script (#339)
    
    * Improved launcher script
    
    Seems ERL_LIBS wasn't picked up universally across versions, so I
    swapped that out for the -pa flag
    
    * silenced errors, used command -v to check for asdf / rtx
    
    * Evalled output from rtx env
    
    * fixed typo
    
    * Update bin/start_lexical.sh
    
    Co-authored-by: Étienne Lévesque <contact@etiennel.dev>
    
    ---------
    
    Co-authored-by: Étienne Lévesque <contact@etiennel.dev>
    
    * fix: update Nix derivation to use new build (#344)
    
    * fix: update Nix derivation to use new build
    
    ---------
    
    Co-authored-by: Steve Cohen <scohen@scohen.org>
    
    ---------
    
    Co-authored-by: Étienne Lévesque <contact@etiennel.dev>
    Co-authored-by: Łukasz Jan Niemier <lukasz@niemier.pl>

--- bin/start_lexical.sh
+++ bin/start_lexical.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 set_up_version_manager() {
-    if [ -e $HOME/.asdf ] &&  asdf which elixir > /dev/null -eq 0; then
+    if command -v asdf > /dev/null && asdf which elixir > /dev/null 2>&1 ; then
         VERSION_MANAGER="asdf"
-    elif [ -e $HOME/.rtx ] && rtx which elixir > /dev/null -eq 0; then
+    elif command -v rtx > /dev/null &&  rtx which elixir > /dev/null 2>&1 ; then
         VERSION_MANAGER="rtx"
     else
         VERSION_MANAGER="none"
     fi
 }
-
 
 set_up_version_manager
 
@@ -19,7 +18,7 @@ case "$VERSION_MANAGER" in
         asdf env erl exec "$@" &
         ;;
     rtx)
-        rtx env -s bash erl exec "$@" &
+        eval "$(rtx env -s bash)"
         ;;
     *)
         ;;
@@ -36,8 +35,14 @@ esac
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export ERL_LIBS="${SCRIPT_DIR}/../lib"
-"${ELIXIR_COMMAND}" -pa "${SCRIPT_DIR}/../consolidated" \
+PATH_APPEND_ARGS=$(for f in $(find ${SCRIPT_DIR}/../lib -name '*.ez')
+do
+    lib=$(basename $f | sed -e 's/.ez//g')
+    echo "-pa $f/$lib/ebin"
+done)
+
+"${ELIXIR_COMMAND}" $(echo $PATH_APPEND_ARGS) \
+                    -pa "${SCRIPT_DIR}/../consolidated" \
                     -pa "${SCRIPT_DIR}/../config/" \
                     -pa "${SCRIPT_DIR}/../priv/" \
                     --eval "LXical.Server.Boot.start" \
