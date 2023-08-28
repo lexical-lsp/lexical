73405edfb732634421b6c609b243581bec2270cb
Zach Allaun <zach.allaun@gmail.com>
2023-08-28 14:16:09 -0400

    Refactor hover test

--- apps/server/test/lexical/server/provider/handlers/hover_test.exs
+++ apps/server/test/lexical/server/provider/handlers/hover_test.exs
@@ -1,43 +1,181 @@
 defmodule Lexical.Server.Provider.Handlers.HoverTest do
   alias Lexical.Document
+  alias Lexical.Document.Position
   alias Lexical.Proto.Convert
   alias Lexical.Protocol.Requests
   alias Lexical.Protocol.Types
+  alias Lexical.RemoteControl
+  alias Lexical.RemoteControl.Api.Messages
   alias Lexical.Server
   alias Lexical.Server.Provider.Env
   alias Lexical.Server.Provider.Handlers
+  alias Lexical.Test.Fixtures
+  alias Lexical.Test.Protocol.Fixtures.LspProtocol
 
-  import Lexical.Test.Protocol.Fixtures.LspProtocol
-  import Lexical.RemoteControl.Api.Messages
-  import Lexical.Test.Fixtures
+  import Lexical.Test.CodeSigil
+  import Lexical.Test.CursorSupport
+
+  require Messages
 
   use ExUnit.Case, async: false
 
   setup_all do
-    project = project()
+    project = Fixtures.project()
 
     {:ok, _} = start_supervised(Document.Store)
     {:ok, _} = start_supervised({DynamicSupervisor, Server.Project.Supervisor.options()})
     {:ok, _} = start_supervised({Server.Project.Supervisor, project})
 
-    :ok = Server.Project.Dispatch.register(project, [project_compiled()])
-    assert_receive project_compiled(), 5000
+    :ok = Server.Project.Dispatch.register(project, [Messages.project_compiled()])
+    assert_receive Messages.project_compiled(), 5000
 
     {:ok, project: project}
   end
 
-  setup [:with_uri]
+  # compiles and writes beam files in the given project
+  defp with_compiled_in(project, code, fun) do
+    tmp_dir = Fixtures.file_path(project, "lib/tmp")
 
-  defp with_uri(%{project: project, uri_for: file}) do
-    path = file_path(project, Path.join("lib", file))
-    %{uri: Document.Path.ensure_uri(path)}
+    tmp_path =
+      tmp_dir
+      |> Path.join("tmp_#{rand_hex(10)}.ex")
+
+    File.mkdir_p!(tmp_dir)
+
+    with_tmp_file(tmp_path, code, fn ->
+      {:ok, compile_path} =
+        RemoteControl.Mix.in_project(project, fn _ ->
+          Mix.Project.compile_path()
+        end)
+
+      {:ok, modules, _} =
+        RemoteControl.call(project, Kernel.ParallelCompiler, :compile_to_path, [
+          [tmp_path],
+          compile_path
+        ])
+
+      try do
+        fun.()
+      after
+        for module <- modules do
+          path = RemoteControl.call(project, :code, :which, [module])
+          RemoteControl.call(project, :code, :delete, [module])
+          File.rm!(path)
+        end
+      end
+    end)
   end
 
-  defp with_uri(_) do
-    :ok
+  defp rand_hex(n_bytes) do
+    n_bytes
+    |> :crypto.strong_rand_bytes()
+    |> Base.hex_encode32()
   end
 
-  def hover_request(path, line, char) do
+  defp with_tmp_file(file, content, fun) do
+    File.write!(file, content)
+    fun.()
+  after
+    File.rm_rf!(file)
+  end
+
+  describe "module hover" do
+    test "replies with public module doc", %{project: project} do
+      code = ~q[
+        defmodule HoverWithDoc do
+          @moduledoc """
+          This module has a moduledoc.
+          """
+        end
+      ]
+
+      with_compiled_in(project, code, fn ->
+        assert {:reply, %{result: %Types.Hover{} = result}} = hover(project, "|HoverWithDoc")
+        assert result.contents.kind == :markdown
+
+        assert result.contents.value == """
+               ### HoverWithDoc
+
+               This module has a moduledoc.
+               """
+      end)
+    end
+
+    test "notes private modules", %{project: project} do
+      code = ~q[
+        defmodule HoverPrivate do
+          @moduledoc false
+        end
+      ]
+
+      with_compiled_in(project, code, fn ->
+        assert {:reply, %{result: %Types.Hover{} = result}} = hover(project, "|HoverPrivate")
+        assert result.contents.kind == :markdown
+
+        assert result.contents.value == """
+               ### HoverPrivate
+
+               *This module is private.*
+               """
+      end)
+    end
+
+    test "notes modules without docs", %{project: project} do
+      code = ~q[
+        defmodule HoverNoDocs do
+        end
+      ]
+
+      with_compiled_in(project, code, fn ->
+        assert {:reply, %{result: %Types.Hover{} = result}} = hover(project, "|HoverNoDocs")
+        assert result.contents.kind == :markdown
+
+        assert result.contents.value == """
+               ### HoverNoDocs
+
+               *This module is undocumented.*
+               """
+      end)
+    end
+  end
+
+  defp hover(project, code) do
+    with {position, code} <- pop_position(code),
+         {:ok, document} <- document_with_content(project, code),
+         {:ok, request} <- hover_request(document.uri, position) do
+      Handlers.Hover.handle(request, %Env{project: project})
+    end
+  end
+
+  defp pop_position(code) do
+    {line, character} = cursor_position(code)
+    {Position.new(line, character), strip_cursor(code)}
+  end
+
+  defp document_with_content(project, content) do
+    uri =
+      project
+      |> Fixtures.file_path(Path.join("lib", "my_doc.ex"))
+      |> Document.Path.ensure_uri()
+
+    case Document.Store.open(uri, content, 1) do
+      :ok ->
+        Document.Store.fetch(uri)
+
+      {:error, :already_open} ->
+        Document.Store.close(uri)
+        document_with_content(project, content)
+
+      error ->
+        error
+    end
+  end
+
+  defp hover_request(path, %Position{} = position) do
+    hover_request(path, position.line, position.character)
+  end
+
+  defp hover_request(path, line, char) do
     uri = Document.Path.ensure_uri(path)
 
     params = [
@@ -46,72 +184,8 @@ defmodule Lexical.Server.Provider.Handlers.HoverTest do
     ]
 
     with {:ok, _} <- Document.Store.open_temporary(uri),
-         {:ok, req} <- build(Requests.Hover, params) do
+         {:ok, req} <- LspProtocol.build(Requests.Hover, params) do
       Convert.to_native(req)
     end
-  end
-
-  def handle(request, project) do
-    Handlers.Hover.handle(request, %Env{project: project})
-  end
-
-  describe "module hover" do
-    @describetag uri_for: "docs.ex"
-
-    test "module with docs", %{project: project, uri: uri} do
-      # alias Project.Docs.PublicModule
-      #                    ^
-      {:ok, request} = hover_request(uri, 64, 21)
-
-      assert {:reply, %{result: %Types.Hover{contents: content}}} = handle(request, project)
-
-      assert content.kind == :markdown
-
-      assert content.value == """
-             ### Project.Docs.PublicModule
-
-             This module has docs.
-             """
-    end
-
-    test "hidden module", %{project: project, uri: uri} do
-      # alias Project.Docs.PrivateModule
-      #                    ^
-      {:ok, request} = hover_request(uri, 69, 21)
-
-      assert {:reply, %{result: %Types.Hover{contents: content}}} = handle(request, project)
-
-      assert content.kind == :markdown
-
-      assert content.value == """
-             ### Project.Docs.PrivateModule
-
-             *This module is private.*
-             """
-    end
-
-    test "undocumented module", %{project: project, uri: uri} do
-      # alias Project.Docs.UndocumentedModule
-      #                    ^
-      {:ok, request} = hover_request(uri, 74, 21)
-
-      assert {:reply, %{result: %Types.Hover{contents: content}}} = handle(request, project)
-
-      assert content.kind == :markdown
-
-      assert content.value == """
-             ### Project.Docs.UndocumentedModule
-
-             *This module is undocumented.*
-             """
-    end
-  end
-
-  @tag uri_for: "docs.ex"
-  test "replies with nil result when there is nothing to hover", %{project: project, uri: uri} do
-    # column well outside a reasonable range
-    {:ok, request} = hover_request(uri, 0, 1_000)
-
-    assert {:reply, %{result: nil}} = handle(request, project)
   end
 end
