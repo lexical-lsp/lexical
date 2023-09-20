9368b359ff821214fd109d58badc3e61b7e7f9df
Steve Cohen <scohen@scohen.org>
2023-09-19 18:42:56 -0700

    ensuring node starts

--- apps/remote_control/test/test_helper.exs
+++ apps/remote_control/test/test_helper.exs
@@ -2,6 +2,9 @@ alias Lexical.RemoteControl.Search.Store
 alias Lexical.RemoteControl.Search.Store.Mnesia
 Store.set_backend(Store.Ets)
 Mnesia.set_persist_to_disc(false)
+
+{"", 0} = System.cmd("epmd", ~w(-daemon))
 random_number = :rand.uniform(500)
-Node.start(:"testing-#{random_number}@127.0.0.1")
+{:ok, _pid} = :net_kernel.start([:"testing-#{random_number}@127.0.0.1"])
+
 ExUnit.start(exclude: [:skip])
