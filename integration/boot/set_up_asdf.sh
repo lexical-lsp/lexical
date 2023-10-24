09b36e1f4e38da50e430be1cbd622b90cc3856cf
Zach Allaun <zach.allaun@gmail.com>
2023-10-24 11:21:56 -0700

    Fix crash on boot due to precedence bug and stdio log (#444)
    The checks in `activate_version_manager.sh` need explicit `{ }` to prevent `_detect_rtx` from always running. This would cause the script to exit with an error spuriously, which would trigger an error message that was being written to stdio. Because we use stdio as our transport, that would cause the LS client to crash.
    
    This fixes the crash reported in #440. However, it will still run the server using asdf if both asdf and rtx are installed with the correct versions of Elixir.

--- integration/boot/set_up_asdf.sh
+++ integration/boot/set_up_asdf.sh
@@ -13,9 +13,9 @@ asdf update
 
 export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac --without-termcap --without-wx"
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf install erlang latest
-asdf global erlang latest
+asdf install erlang "$ERLANG_VERSION"
+asdf global erlang "$ERLANG_VERSION"
 
 asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
-asdf install elixir latest
-asdf global elixir latest
+asdf install elixir "$ELIXIR_VERSION"
+asdf global elixir "$ELIXIR_VERSION"
