1eeb7715e2eb2a21795801cb002798f055ade3aa
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-11-15 16:37:30 -0300

    config: disables JIT on erlang/OTP only on macOS

--- flake.nix
+++ flake.nix
@@ -4,26 +4,41 @@
   inputs.nixpkgs.url = "flake:nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        erl = pkgs.beam.packages.erlang;
-        lexical = erl.callPackage ./nix/lexical.nix {};
-      in
-      {
+      system: let
+        # little hack as we can't use `pkgs.system` yet
+        is_darwin = system == "aarch64-darwin" || system == "x86_64-darwin";
+        pkgs = import nixpkgs {inherit system;};
+        inherit (pkgs.beam) packagesWith;
+        inherit (pkgs.beam.interpreters) erlang;
+        # yeah, it will take a long time to build...
+        # nedeed because macOS sonoma breaks the JIT
+        # reference: https://elixirforum.com/t/bus-error-after-upgrading-to-sonoma-beta/56354/44
+        # another option is to use erlang/OTP >= 23.3.2.7 which disables JIT automatically on macOS
+        darwin_override_erl = erlang.overrideAttrs (old: {
+          configureFlags = old.configureFlags ++ ["--disable-jit"];
+        });
+
+        erl =
+          if is_darwin
+          then darwin_override_erl
+          else erlang;
+
+        erl' = packagesWith erl;
+        lexical = erl'.callPackage ./nix/lexical.nix {};
+      in {
         packages = {
           inherit lexical;
 
           default = lexical;
 
           # Private package used to automatically generate hash for Mix deps
-          __fodHashGen = lexical.mixFodDeps.overrideAttrs(final: curr: {
+          __fodHashGen = lexical.mixFodDeps.overrideAttrs (final: curr: {
             outputHash = pkgs.lib.fakeSha256;
           });
         };
