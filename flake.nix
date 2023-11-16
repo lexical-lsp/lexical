e6408dcedd154e6e865889ea33e04ff70a29b4bd
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-11-16 11:25:05 -0300

    feat: parametrize erlang to build lexical-lsp

--- flake.nix
+++ flake.nix
@@ -11,32 +11,18 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        # little hack as we can't use `pkgs.system` yet
-        is_darwin = system == "aarch64-darwin" || system == "x86_64-darwin";
         pkgs = import nixpkgs {inherit system;};
-        inherit (pkgs.beam) packagesWith;
-        inherit (pkgs.beam.interpreters) erlang;
-        # yeah, it will take a long time to build...
-        # nedeed because macOS sonoma breaks the JIT
-        # reference: https://elixirforum.com/t/bus-error-after-upgrading-to-sonoma-beta/56354/44
-        # another option is to use erlang/OTP >= 23.3.2.7 which disables JIT automatically on macOS
-        darwin_override_erl = erlang.overrideAttrs (old: {
-          configureFlags = old.configureFlags ++ ["--disable-jit"];
-        });
+        inherit (pkgs.beam.packages) erlang_26;
 
-        erl =
-          if is_darwin
-          then darwin_override_erl
-          else erlang;
-
-        erl' = packagesWith erl;
-        lexical = erl'.callPackage ./nix/lexical.nix {};
-      in {
-        packages = {
-          inherit lexical;
-
+        mkLexical = {erlang}: erlang.callPackage ./nix/lexical.nix {};
+      in rec {
+        overlay = _final: _prev: {
+          inherit mkLexical;
+          inherit (packages) lexical;
+        };
+        packages = rec {
+          lexical = mkLexical {erlang = erlang_26;};
           default = lexical;
-
           # Private package used to automatically generate hash for Mix deps
           __fodHashGen = lexical.mixFodDeps.overrideAttrs (final: curr: {
             outputHash = pkgs.lib.fakeSha256;
@@ -44,13 +30,11 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages =
-            [
-              erl.elixir
-            ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-              pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-              pkgs.darwin.apple_sdk.frameworks.CoreServices
+          packages = with pkgs;
+            [erlang_26.elixir]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.CoreServices
             ];
         };
       }
