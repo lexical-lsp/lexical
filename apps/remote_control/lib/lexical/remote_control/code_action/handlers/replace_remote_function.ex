d085f346d075a7cca6a97a4f44fc2e0649db4d86
Samuel Hełdak <samuel.heldak@gmail.com>
2023-11-21 19:59:52 +0100

    Add replacing unknown remote function to code actions (#443)
    * Add replacing unknown remote function to code actions
    
    * Update after review
    
    * update extracting line
    
    * Bring back separate extract_function

---
+++ apps/remote_control/lib/lexical/remote_control/code_action/handlers/replace_remote_function.ex
@@ -0,0 +1,123 @@
+defmodule Lexical.RemoteControl.CodeAction.Handlers.ReplaceRemoteFunction do
+  alias Lexical.Ast
+  alias Lexical.Document
+  alias Lexical.Document.Changes
+  alias Lexical.Document.Edit
+  alias Lexical.Document.Range
+  alias Lexical.RemoteControl.CodeAction
+  alias Lexical.RemoteControl.CodeAction.Diagnostic
+  alias Sourceror.Zipper
+
+  @behaviour CodeAction.Handler
+
+  @impl CodeAction.Handler
+  def actions(%Document{} = doc, %Range{}, diagnostics) do
+    Enum.flat_map(diagnostics, fn %Diagnostic{} = diagnostic ->
+      with {:ok, module, function, arity, line_number} <- extract_function_and_line(diagnostic),
+           {:ok, suggestions} <- prepare_suggestions(module, function, arity) do
+        to_code_actions(doc, line_number, module, function, suggestions)
+      else
+        _ ->
+          []
+      end
+    end)
+  end
+
+  @impl CodeAction.Handler
+  def kinds do
+    [:quick_fix]
+  end
+
+  @spec to_code_actions(Document.t(), non_neg_integer(), module(), String.t(), [atom()]) ::
+          [CodeAction.t()]
+  defp to_code_actions(%Document{} = doc, line_number, module, function, suggestions) do
+    suggestions
+    |> Enum.reduce([], fn suggestion, acc ->
+      case apply_transform(doc, line_number, module, function, suggestion) do
+        {:ok, edits} ->
+          changes = Changes.new(doc, edits)
+          code_action = CodeAction.new(doc.uri, "Rename to #{suggestion}", :quick_fix, changes)
+
+          [code_action | acc]
+
+        :error ->
+          acc
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  @spec apply_transform(Document.t(), non_neg_integer(), module(), String.t(), atom()) ::
+          {:ok, [Edit.t()]} | :error
+  defp apply_transform(%Document{} = doc, line_number, module, function, suggestion) do
+    function_atom = String.to_atom(function)
+
+    doc
+    |> Ast.traverse_line(line_number, [], fn
+      %Zipper{node: {{:., _, [{:__aliases__, _, module_alias}, ^function_atom]}, _, _} = node} =
+          zipper,
+      patches ->
+        case Lexical.Ast.expand_aliases(module_alias, doc, {line_number, 0}) do
+          {:ok, ^module} ->
+            [patch] = Sourceror.Patch.rename_call(node, suggestion)
+            {zipper, [patch | patches]}
+
+          _ ->
+            {zipper, patches}
+        end
+
+      zipper, patches ->
+        {zipper, patches}
+    end)
+    |> case do
+      {:ok, _zipper, patches} ->
+        Ast.patches_to_edits(doc, patches)
+
+      _ ->
+        :error
+    end
+  end
+
+  defp extract_function_and_line(%Diagnostic{} = diagnostic) do
+    with {:ok, module, function, arity} <- extract_function(diagnostic.message) do
+      {:ok, module, function, arity, diagnostic.range.start.line}
+    end
+  end
+
+  @function_re ~r/(.*)\/(.*) is undefined or private. Did you mean:.*/
+  defp extract_function(message) do
+    with [[_, module_and_function, arity]] <- Regex.scan(@function_re, message),
+         {:ok, module, function_name} <- separate_module_from_function(module_and_function) do
+      {:ok, module, function_name, String.to_integer(arity)}
+    end
+  end
+
+  defp separate_module_from_function(module_and_function) do
+    module_and_function
+    |> String.split(".")
+    |> List.pop_at(-1)
+    |> case do
+      {function_name, [_ | _] = module_alias} ->
+        {:ok, Module.concat(module_alias), function_name}
+
+      _ ->
+        :error
+    end
+  end
+
+  @function_threshold 0.77
+  @max_suggestions 5
+  defp prepare_suggestions(module, function, arity) do
+    suggestions =
+      for {module_function, ^arity} <- module.__info__(:functions),
+          distance = module_function |> Atom.to_string() |> String.jaro_distance(function),
+          distance >= @function_threshold do
+        {distance, module_function}
+      end
+      |> Enum.sort(:desc)
+      |> Enum.take(@max_suggestions)
+      |> Enum.map(fn {_distance, module_function} -> module_function end)
+
+    {:ok, suggestions}
+  end
+end
---
+++ apps/remote_control/test/lexical/remote_control/code_action/handlers/replace_remote_function_test.exs
@@ -0,0 +1,224 @@
+defmodule Lexical.RemoteControl.CodeAction.Handlers.ReplaceRemoteFunctionTest do
+  alias Lexical.Document
+  alias Lexical.RemoteControl.CodeAction.Diagnostic
+  alias Lexical.RemoteControl.CodeAction.Handlers.ReplaceRemoteFunction
+
+  use Lexical.Test.CodeMod.Case
+
+  def apply_code_mod(original_text, _ast, options) do
+    line_number = Keyword.get(options, :line, 1)
+
+    suggestion =
+      options
+      |> Keyword.get(:suggestion, :count)
+      |> Atom.to_string()
+
+    document = Document.new("file:///file.ex", original_text, 0)
+
+    message =
+      """
+      Enum.counts/1 is undefined or private. Did you mean:
+
+            * concat/1
+            * concat/2
+            * count/1
+            * count/2
+      """
+      |> String.trim()
+
+    range =
+      Document.Range.new(
+        Document.Position.new(document, line_number, 0),
+        Document.Position.new(document, line_number + 1, 0)
+      )
+
+    diagnostic = Diagnostic.new(range, message, nil)
+
+    changes =
+      document
+      |> ReplaceRemoteFunction.actions(range, [diagnostic])
+      |> Enum.flat_map(& &1.changes.edits)
+      |> Enum.filter(fn
+        %Lexical.Document.Edit{text: ^suggestion} -> true
+        _ -> false
+      end)
+
+    {:ok, changes}
+  end
+
+  describe "fixes function call" do
+    test "applied to a standalone call" do
+      {:ok, result} =
+        ~q{
+          Enum.counts([1, 2, 3])
+        }
+        |> modify()
+
+      assert result == "Enum.count([1, 2, 3])"
+    end
+
+    test "applied to a variable match" do
+      {:ok, result} =
+        ~q{
+          x = Enum.counts([1, 2, 3])
+        }
+        |> modify()
+
+      assert result == "x = Enum.count([1, 2, 3])"
+    end
+
+    test "applied to a variable match, preserves comments" do
+      {:ok, result} =
+        ~q{
+          x = Enum.counts([1, 2, 3]) # TODO: Fix this
+        }
+        |> modify()
+
+      assert result == "x = Enum.count([1, 2, 3]) # TODO: Fix this"
+    end
+
+    test "not changing variable name" do
+      {:ok, result} =
+        ~q{
+          counts = Enum.counts([1, 2, 3])
+        }
+        |> modify()
+
+      assert result == "counts = Enum.count([1, 2, 3])"
+    end
+
+    test "applied to a call after a pipe" do
+      {:ok, result} =
+        ~q{
+          [1, 2, 3] |> Enum.counts()
+        }
+        |> modify()
+
+      assert result == "[1, 2, 3] |> Enum.count()"
+    end
+
+    test "changing only a function from provided possible modules" do
+      {:ok, result} =
+        ~q{
+          Enumerable.counts([1, 2, 3]) + Enum.counts([3, 2, 1])
+        }
+        |> modify()
+
+      assert result == "Enumerable.counts([1, 2, 3]) + Enum.count([3, 2, 1])"
+    end
+
+    test "changing all occurrences of the function in the line" do
+      {:ok, result} =
+        ~q{
+          Enum.counts([1, 2, 3]) + Enum.counts([3, 2, 1])
+        }
+        |> modify()
+
+      assert result == "Enum.count([1, 2, 3]) + Enum.count([3, 2, 1])"
+    end
+
+    test "applied in a comprehension" do
+      {:ok, result} =
+        ~q{
+          for x <- Enum.counts([[1], [2], [3]]), do: x
+        }
+        |> modify(suggestion: :concat)
+
+      assert result == "for x <- Enum.concat([[1], [2], [3]]), do: x"
+    end
+
+    test "applied in a with block" do
+      {:ok, result} =
+        ~q{
+          with x <- Enum.counts([1, 2, 3]), do: x
+        }
+        |> modify()
+
+      assert result == "with x <- Enum.count([1, 2, 3]), do: x"
+    end
+
+    test "preserving the leading indent" do
+      {:ok, result} = modify("     Enum.counts([1, 2, 3])", trim: false)
+
+      assert result == "     Enum.count([1, 2, 3])"
+    end
+  end
+
+  describe "fixes captured function" do
+    test "applied to a standalone function" do
+      {:ok, result} =
+        ~q[
+          &Enum.counts/1
+        ]
+        |> modify()
+
+      assert result == "&Enum.count/1"
+    end
+
+    test "applied to a variable match" do
+      {:ok, result} =
+        ~q[
+          x = &Enum.counts/1
+        ]
+        |> modify()
+
+      assert result == "x = &Enum.count/1"
+    end
+
+    test "applied to a variable match, preserves comments" do
+      {:ok, result} =
+        ~q[
+          x = &Enum.counts/1 # TODO: Fix this
+        ]
+        |> modify()
+
+      assert result == "x = &Enum.count/1 # TODO: Fix this"
+    end
+
+    test "not changing variable name" do
+      {:ok, result} =
+        ~q{
+          counts = &Enum.counts/1
+        }
+        |> modify()
+
+      assert result == "counts = &Enum.count/1"
+    end
+
+    test "applied to an argument" do
+      {:ok, result} =
+        ~q{
+          [[1, 2], [3, 4]] |> Enum.map(&Enum.counts/1)
+        }
+        |> modify()
+
+      assert result == "[[1, 2], [3, 4]] |> Enum.map(&Enum.count/1)"
+    end
+
+    test "changing only a function from provided possible modules" do
+      {:ok, result} =
+        ~q{
+          [&Enumerable.counts/1, &Enum.counts/1]
+        }
+        |> modify()
+
+      assert result == "[&Enumerable.counts/1, &Enum.count/1]"
+    end
+
+    test "changing all occurrences of the function in the line" do
+      {:ok, result} =
+        ~q{
+          [&Enum.counts/1, &Enum.counts/1]
+        }
+        |> modify()
+
+      assert result == "[&Enum.count/1, &Enum.count/1]"
+    end
+
+    test "preserving the leading indent" do
+      {:ok, result} = modify("     &Enum.counts/1", trim: false)
+
+      assert result == "     &Enum.count/1"
+    end
+  end
+end
