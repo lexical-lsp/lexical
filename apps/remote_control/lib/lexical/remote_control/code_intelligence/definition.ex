58fe8d348127a0c7844c7939ffc8b89174b54a6e
Scott Ming <therealscottming@gmail.com>
2024-01-01 19:45:35 +0800

    Upgrade ElixirSense
    Also Fix the wrong definition tests

--- apps/remote_control/lib/lexical/remote_control/code_intelligence/definition.ex
+++ apps/remote_control/lib/lexical/remote_control/code_intelligence/definition.ex
@@ -1,5 +1,6 @@
 defmodule Lexical.RemoteControl.CodeIntelligence.Definition do
   alias Future.Code, as: Code
+  alias Lexical.Ast
   alias Lexical.Document
   alias Lexical.Document.Location
   alias Lexical.Document.Position
@@ -14,12 +15,13 @@ defmodule Lexical.RemoteControl.CodeIntelligence.Definition do
   end
 
   defp parse_location(%ElixirSense.Location{} = location, document) do
-    %{file: file, line: line, column: column} = location
+    %{file: file, line: line, column: column, type: type} = location
     file_path = file || document.path
     uri = Document.Path.ensure_uri(file_path)
 
     with {:ok, document} <- Document.Store.open_temporary(uri),
          {:ok, text} <- Document.fetch_text_at(document, line) do
+      {line, column} = maybe_move_cursor_to_next(type, document, line, column)
       range = to_precise_range(document, text, line, column)
       {:ok, Location.new(range, document)}
     else
@@ -31,6 +33,28 @@ defmodule Lexical.RemoteControl.CodeIntelligence.Definition do
   defp parse_location(nil, _) do
     {:ok, nil}
   end
+
+  defp maybe_move_cursor_to_next(type, document, line, column)
+       when type in [:function, :module, :macro] do
+    position = Position.new(document, line, column)
+
+    case Ast.zipper_at(document, position) do
+      {:ok, zipper} ->
+        zipper = %{node: {entity_name, meta, _}} = Sourceror.Zipper.next(zipper)
+
+        if entity_name == :when do
+          %{node: {_entity_name, meta, _}} = Sourceror.Zipper.next(zipper)
+          {meta[:line], meta[:column]}
+        else
+          {meta[:line], meta[:column]}
+        end
+
+      _ ->
+        {line, column}
+    end
+  end
+
+  defp maybe_move_cursor_to_next(_, _, line, column), do: {line, column}
 
   defp to_precise_range(%Document{} = document, text, line, column) do
     case Code.Fragment.surround_context(text, {line, column}) do
