194b7d18fbe647424ca1f5c7de88e59490b120bf
Steve Cohen <scohen@scohen.org>
2024-01-04 11:29:47 -0800

    Metadata now recognizes the block ranges of terse functions
    While working on the structure PR, I noticed that the indexer was
    returning both definitions and references for functions defined
    tersely, that is without a do/end. For example:
    
    ```elixir
    def add_one(arg), do: arg + 1
    ```
    
    Somewhat surprisingly, this is because the metadata location function
    was not correctly recognizing this as a block, and was returning a
    single position. The block code thought this was just an expression
    and they weren't properly registered as blocks, but the indexer
    correctly recognized them as definitions.
    
    This PR improves detection of blocks by the metadata to recognize
    multiple types of blocks, whether or not they're terse.
    
    Fixes #540

--- apps/remote_control/lib/lexical/remote_control/search/indexer/metadata.ex
+++ apps/remote_control/lib/lexical/remote_control/search/indexer/metadata.ex
@@ -2,14 +2,14 @@ defmodule Lexical.RemoteControl.Search.Indexer.Metadata do
   @moduledoc """
   Utilities for extracting location information from AST metadata nodes.
   """
-  def location({_, metadata, _}) do
+  def location({_, metadata, _} = ast) do
     if Keyword.has_key?(metadata, :do) do
       position = position(metadata)
       block_start = position(metadata, :do)
       block_end = position(metadata, :end_of_expression) || position(metadata, :end)
       {:block, position, block_start, block_end}
     else
-      {:expression, position(metadata)}
+      maybe_handle_terse_function(ast)
     end
   end
 
@@ -34,5 +34,53 @@ defmodule Lexical.RemoteControl.Search.Indexer.Metadata do
     keyword
     |> Keyword.get(key, [])
     |> position()
+  end
+
+  @defines [:def, :defp, :defmacro, :defmacrop]
+  # a terse function is one without a do/end block
+  defp maybe_handle_terse_function({define, metadata, [_name_and_args | [[block | _]]]})
+       when define in @defines do
+    case Sourceror.get_range(block) do
+      %{start: start_pos, end: end_pos} ->
+        position = position(metadata)
+
+        {:block, position, keyword_to_position(start_pos), keyword_to_position(end_pos)}
+
+      _ ->
+        {:expression, position(metadata)}
+    end
+  end
+
+  defp maybe_handle_terse_function({:fn, metadata, _} = ast) do
+    case Sourceror.get_range(ast) do
+      %{start: start_pos, end: end_pos} ->
+        position = position(metadata)
+        [line: line, column: column] = Keyword.take(end_pos, [:line, :column])
+        # There is a bug in sourceror that doesn't handle anonymous functions ranges
+        # correctly, placing the end position at the 'n' in the 'end'. This works
+        # around it.
+        end_position = {line, column - 1}
+
+        {:block, position, keyword_to_position(start_pos), end_position}
+
+      _ ->
+        {:expression, position(metadata)}
+    end
+  end
+
+  defp maybe_handle_terse_function({_, metadata, _}) do
+    {:expression, position(metadata)}
+  end
+
+  defp keyword_to_position(nil), do: nil
+
+  defp keyword_to_position(keyword) do
+    case Keyword.take(keyword, [:line, :column]) do
+      [line: line, column: column] when is_number(line) and is_number(column) ->
+        {line, column}
+
+      _ ->
+        nil
+    end
   end
 end
---
+++ apps/remote_control/test/lexical/remote_control/search/indexer/metadata_test.exs
@@ -0,0 +1,200 @@
+defmodule Lexical.RemoteControl.Search.Indexer.MetadataTest do
+  alias Lexical.Ast
+  alias Lexical.Document
+  alias Lexical.Document.Position
+  alias Lexical.Document.Range
+  alias Lexical.RemoteControl.Search.Indexer.Metadata
+
+  use ExUnit.Case
+
+  import Lexical.Test.RangeSupport
+  import Lexical.Test.CodeSigil
+
+  describe "blocks in modules" do
+    test "finds a block in an empty module" do
+      code = ~q[
+        defmodule MyModule do
+        end
+      ]t
+
+      assert "defmodule MyModule «do\n»end" == decorate_location(code)
+    end
+
+    test "finds a block in a module with an attribute" do
+      code = ~q[
+        defmodule WithAttribute do
+          @foo 32
+        end
+      ]t
+
+      assert "defmodule WithAttribute «do\n  @foo 32\n»end" = decorate_location(code)
+    end
+
+    test "finds a block in a module with functions" do
+      code = ~q[
+        defmodule WithFunctions do
+          def fun do
+          end
+        end
+      ]t
+
+      expected = ~q[
+        defmodule WithFunctions «do
+          def fun do
+          end
+        »end
+      ]t
+      assert expected == decorate_location(code)
+    end
+  end
+
+  describe "blocks in functions" do
+    test "are found in a public function with do/end and no body" do
+      code = ~q[
+      def my_fn do
+      end
+      ]
+      assert "def my_fn «do\n»end" = decorate_location(code)
+    end
+
+    test "are found in a private function with do/end and no body" do
+      code = ~q[
+      defp my_fn do
+      end
+      ]
+      assert "defp my_fn «do\n»end" = decorate_location(code)
+    end
+
+    test "are found in a public function with do/end and  abody" do
+      code = ~q[
+      def my_fn do
+        x = 4
+        x * 6
+      end
+      ]t
+
+      expected = ~q[
+      def my_fn «do
+        x = 4
+        x * 6
+      »end
+      ]t
+
+      assert expected == decorate_location(code)
+    end
+
+    test "are found in a private function with do/end and  abody" do
+      code = ~q[
+      defp my_fn do
+        x = 4
+        x * 6
+      end
+      ]t
+
+      expected = ~q[
+      defp my_fn «do
+        x = 4
+        x * 6
+      »end
+      ]t
+
+      assert expected == decorate_location(code)
+    end
+
+    test "are found in single line terse public function" do
+      code = ~q[
+        def my_fn(arg), do: arg * 10
+      ]t
+
+      assert "def my_fn(arg), «do: arg * 10»" = decorate_location(code)
+    end
+
+    test "are found in single line terse private function" do
+      code = ~q[
+        defp my_fn(arg), do: arg * 10
+      ]t
+
+      assert "defp my_fn(arg), «do: arg * 10»" = decorate_location(code)
+    end
+
+    test "are found in single line terse public macro" do
+      code = ~q[
+        defmacro my_fn(arg), do: arg * 10
+      ]t
+
+      assert "defmacro my_fn(arg), «do: arg * 10»" = decorate_location(code)
+    end
+
+    test "are found in single line terse private macro" do
+      code = ~q[
+        defmacrop my_fn(arg), do: arg * 10
+      ]t
+
+      assert "defmacrop my_fn(arg), «do: arg * 10»" = decorate_location(code)
+    end
+
+    test "are found in multiple line terse public function" do
+      code = ~q[
+      def my_fun(arg),
+        do: arg + 1
+      ]
+
+      assert "  «do: arg + 1»" == decorate_location(code)
+    end
+
+    test "are found in multiple line terse private function" do
+      code = ~q[
+      defp my_fun(arg),
+        do: arg + 1
+      ]
+
+      assert "  «do: arg + 1»" == decorate_location(code)
+    end
+
+    test "are found in anonymous functions with no body" do
+      code = "fn -> :ok end"
+      assert "«fn -> :ok »end" == decorate_location(code)
+    end
+
+    test "are found in single line anonymous functions" do
+      code = "fn arg -> arg + 1  end"
+      assert "«fn arg -> arg + 1  »end" == decorate_location(code)
+    end
+
+    test "are found in multiple line anonymous functions" do
+      code = ~q[
+        fn a, b ->
+          y = do_something_with(a)
+          z = do_something_with(b)
+          y + z
+        end
+      ]
+
+      expected = ~q[
+        «fn a, b ->
+          y = do_something_with(a)
+          z = do_something_with(b)
+          y + z
+        »end
+      ]t
+
+      assert expected == decorate_location(code)
+    end
+  end
+
+  defp decorate_location(code) do
+    document = Document.new("file:///file.ex", code, 1)
+    {:ok, ast} = Ast.from(document)
+
+    case Metadata.location(ast) do
+      {:block, _position, {start_line, start_char}, {end_line, end_char}} ->
+        range =
+          Range.new(
+            Position.new(document, start_line, start_char),
+            Position.new(document, end_line, end_char)
+          )
+
+        decorate(code, range)
+    end
+  end
+end
