e4a13231b8f5c301cac892b1e7429a9059c8e961
Steve Cohen <scohen@scohen.org>
2024-01-15 19:44:12 -0800

    Ensured snowflake shows up in our apps, addressed aliases as well

--- apps/remote_control/lib/mix/tasks/namespace/transform/configs.ex
+++ apps/remote_control/lib/mix/tasks/namespace/transform/configs.ex
@@ -6,6 +6,9 @@ defmodule Mix.Tasks.Namespace.Transform.Configs do
     |> Path.join("**")
     |> Path.wildcard()
     |> Enum.map(&Path.absname/1)
+    |> tap(fn paths ->
+      Mix.Shell.IO.info("Rewriting #{length(paths)} config scripts.")
+    end)
     |> Enum.each(&apply/1)
   end
 
@@ -15,6 +18,16 @@ defmodule Mix.Tasks.Namespace.Transform.Configs do
       |> File.read!()
       |> Code.string_to_quoted!()
       |> Macro.postwalk(fn
+        {:__aliases__, meta, alias} ->
+          namespaced_alias =
+            alias
+            |> Module.concat()
+            |> Namespace.Module.apply()
+            |> Module.split()
+            |> Enum.map(&String.to_atom/1)
+
+          {:__aliases__, meta, namespaced_alias}
+
         atom when is_atom(atom) ->
           Namespace.Module.apply(atom)
 
