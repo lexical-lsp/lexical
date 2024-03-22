26d41a2b3e7e50ea77b2b27ae6e7bf1bae632224
Scott Ming <therealscottming@gmail.com>
2024-03-22 08:38:33 +0800

    Apply the `defdelegate` suggestion

--- apps/remote_control/lib/lexical/remote_control/code_mod/rename.ex
+++ apps/remote_control/lib/lexical/remote_control/code_mod/rename.ex
@@ -7,18 +7,16 @@ defmodule Lexical.RemoteControl.CodeMod.Rename do
 
   @spec prepare(Analysis.t(), Position.t()) ::
           {:ok, {atom(), String.t()}, Range.t()} | {:error, term()}
-  def prepare(%Analysis{} = analysis, %Position{} = position) do
-    Rename.Prepare.prepare(analysis, position)
-  end
+  defdelegate prepare(analysis, position), to: Rename.Prepare
 
   @rename_mapping %{module: Rename.Module}
 
   @spec rename(Analysis.t(), Position.t(), String.t()) ::
           {:ok, %{Lexical.uri() => [Edit.t()]}} | {:error, term()}
   def rename(%Analysis{} = analysis, %Position{} = position, new_name) do
-    with {:ok, renamable, entity, range} <- Rename.Prepare.resolve(analysis, position) do
-        rename_module = @rename_mapping[renamable]
-        {:ok, rename_module.rename(range, new_name, entity)}
+    with {:ok, {renamable, entity}, range} <- Rename.Prepare.resolve(analysis, position) do
+      rename_module = @rename_mapping[renamable]
+      {:ok, rename_module.rename(range, new_name, entity)}
     end
   end
 end
