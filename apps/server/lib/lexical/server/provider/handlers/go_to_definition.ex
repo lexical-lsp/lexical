37b7eef7b98ac0004a997b241fd705f9185dfb27
Scott Ming <therealscottming@gmail.com>
2024-03-25 15:45:26 +0800

    For module definitions, use the Indexer version instead of `ElixirSense`
    Fixes #649

--- apps/server/lib/lexical/server/provider/handlers/go_to_definition.ex
+++ apps/server/lib/lexical/server/provider/handlers/go_to_definition.ex
@@ -1,4 +1,6 @@
 defmodule Lexical.Server.Provider.Handlers.GoToDefinition do
+  alias Lexical.Ast
+  alias Lexical.Document
   alias Lexical.Protocol.Requests.GoToDefinition
   alias Lexical.Protocol.Responses
   alias Lexical.RemoteControl
@@ -6,13 +8,14 @@ defmodule Lexical.Server.Provider.Handlers.GoToDefinition do
   require Logger
 
   def handle(%GoToDefinition{} = request, env) do
-    case RemoteControl.Api.definition(env.project, request.document, request.position) do
-      {:ok, native_location} ->
-        {:reply, Responses.GoToDefinition.new(request.id, native_location)}
-
+    with {:ok, _document, %Ast.Analysis{valid?: true} = analysis} <-
+           Document.Store.fetch(request.document.uri, :analysis),
+         {:ok, location} <- RemoteControl.Api.definition(env.project, analysis, request.position) do
+      {:reply, Responses.GoToDefinition.new(request.id, location)}
+    else
       {:error, reason} ->
         Logger.error("GoToDefinition failed: #{inspect(reason)}")
-        {:error, Responses.GoToDefinition.error(request.id, :request_failed, inspect(reason))}
+        {:reply, Responses.GoToDefinition.error(request.id, :request_failed, inspect(reason))}
     end
   end
 end
