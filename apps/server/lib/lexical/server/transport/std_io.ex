1f0ff3b18d6bb87f404b3a1e8e8efbef73acfcd6
Moosieus <cameron.duley99@gmail.com>
2024-04-02 12:30:39 -0400

    Improve error handling in `Transport.StdIO`. * Log any errors reading the protocol as critical. * Never split header lines into more than 2 parts. * Replace `header_value/3` with `content_length/1` for more specific errors. * Add note about empty LSP Responses, sent from the language client.

--- apps/server/lib/lexical/server/transport/std_io.ex
+++ apps/server/lib/lexical/server/transport/std_io.ex
@@ -1,6 +1,8 @@
 defmodule Lexical.Server.Transport.StdIO do
   alias Lexical.Protocol.JsonRpc
 
+  require Logger
+
   @behaviour Lexical.Server.Transport
 
   def start_link(device, callback) do
@@ -59,11 +61,16 @@ defmodule Lexical.Server.Transport.StdIO do
       "\n" ->
         headers = parse_headers(buffer)
 
-        with {:ok, content_length} <-
-               header_value(headers, "content-length", &String.to_integer/1),
+        with {:ok, content_length} <- content_length(headers),
              {:ok, data} <- read_body(device, content_length),
              {:ok, message} <- JsonRpc.decode(data) do
           callback.(message)
+        else
+          {:error, :empty_response} ->
+            nil
+
+          {:error, reason} ->
+            Logger.critical("error reading protocol message: #{inspect(reason)}")
         end
 
         loop([], device, callback)
@@ -76,29 +83,62 @@ defmodule Lexical.Server.Transport.StdIO do
     end
   end
 
-  defp parse_headers(headers) do
-    Enum.map(headers, &parse_header/1)
-  end
+  defp content_length(headers) do
+    case List.keyfind(headers, "content-length", 0) do
+      {_, len_str} ->
+        {:ok, String.to_integer(len_str)}
 
-  defp header_value(headers, header_name, converter) do
-    case List.keyfind(headers, header_name, 0) do
-      nil -> :error
-      {_, value} -> {:ok, converter.(value)}
+      nil ->
+        {:error, :no_content_length}
     end
   end
 
   defp read_body(device, byte_count) do
     case IO.binread(device, byte_count) do
-      data when is_binary(data) or is_list(data) ->
+      data when is_binary(data) and byte_size(data) === byte_count ->
         {:ok, data}
 
-      other ->
-        other
+      data when is_binary(data) ->
+        read_more(device, byte_count - byte_size(data), data)
+
+      data when is_list(data) ->
+        if Enum.count(data) === byte_count do
+          {:ok, data}
+        else
+          remaining_bytes = byte_count - byte_size(data)
+          read_more(device, remaining_bytes, data)
+        end
+
+      :eof ->
+        {:error, :eof}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
+  # give editor an extra ms if there's not enough bytes.
+  defp read_more(device, byte_count, data) do
+    :timer.sleep(1)
+
+    case IO.binread(device, byte_count) do
+      rest when is_binary(rest) or is_list(rest) ->
+        [data | rest]
+
+      :eof ->
+        {:error, :eof}
+
+      error ->
+        error
+    end
+  end
+
+  defp parse_headers(headers) do
+    Enum.map(headers, &parse_header/1)
+  end
+
   defp parse_header(line) do
-    [name, value] = String.split(line, ":")
+    [name, value] = String.split(line, ":", parts: 2)
 
     header_name =
       name
