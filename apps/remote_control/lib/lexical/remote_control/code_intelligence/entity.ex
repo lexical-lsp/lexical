bd57ac8ac295c7275397c363e73a7de83e0aa7fa
Scott Ming <therealscottming@gmail.com>
2024-05-02 15:56:09 +0800

    Wrap the cryptic match in a function

--- apps/remote_control/lib/lexical/remote_control/code_intelligence/entity.ex
+++ apps/remote_control/lib/lexical/remote_control/code_intelligence/entity.ex
@@ -481,7 +481,7 @@ defmodule Lexical.RemoteControl.CodeIntelligence.Entity do
   defp maybe_local_capture_func(analysis, position) do
     with {:ok, %Zipper{node: {:/, _, [_, {:__block__, _, _}]}} = zipper} <-
            Ast.zipper_at(analysis.document, position),
-         %Zipper{node: {:&, _, _}} <- Zipper.up(zipper) do
+         true <- inside_capture?(zipper) do
       {:/, _, [{local_func_name, _meta, _}, {:__block__, _, [arity]}]} = zipper.node
       function_name_length = local_func_name |> to_string() |> String.length()
       range = Ast.Range.fetch!(zipper.node, analysis.document)
@@ -492,6 +492,13 @@ defmodule Lexical.RemoteControl.CodeIntelligence.Entity do
     else
       _ ->
         {:error, :not_found}
+    end
+  end
+
+  defp inside_capture?(zipper) do
+    case Zipper.up(zipper) do
+      %Zipper{node: {:&, _, _}} -> true
+      _ -> false
     end
   end
 end
