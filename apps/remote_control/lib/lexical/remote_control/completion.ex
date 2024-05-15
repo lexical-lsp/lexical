442037a49c0b6348d61bd568c7a5fe8a9dc2b34a
Zach Allaun <zach.allaun@gmail.com>
2024-05-15 15:16:13 -0400

    Exclude parens for all built-in locals without parens

--- apps/remote_control/lib/lexical/remote_control/completion.ex
+++ apps/remote_control/lib/lexical/remote_control/completion.ex
@@ -9,6 +9,91 @@ defmodule Lexical.RemoteControl.Completion do
 
   import Document.Line
 
+  @built_in_locals_without_parens [
+    # Special forms
+    alias: 1,
+    alias: 2,
+    case: 2,
+    cond: 1,
+    for: :*,
+    import: 1,
+    import: 2,
+    quote: 1,
+    quote: 2,
+    receive: 1,
+    require: 1,
+    require: 2,
+    try: 1,
+    with: :*,
+
+    # Kernel
+    def: 1,
+    def: 2,
+    defp: 1,
+    defp: 2,
+    defguard: 1,
+    defguardp: 1,
+    defmacro: 1,
+    defmacro: 2,
+    defmacrop: 1,
+    defmacrop: 2,
+    defmodule: 2,
+    defdelegate: 2,
+    defexception: 1,
+    defoverridable: 1,
+    defstruct: 1,
+    destructure: 2,
+    raise: 1,
+    raise: 2,
+    reraise: 2,
+    reraise: 3,
+    if: 2,
+    unless: 2,
+    use: 1,
+    use: 2,
+
+    # Stdlib,
+    defrecord: 2,
+    defrecord: 3,
+    defrecordp: 2,
+    defrecordp: 3,
+
+    # Testing
+    assert: 1,
+    assert: 2,
+    assert_in_delta: 3,
+    assert_in_delta: 4,
+    assert_raise: 2,
+    assert_raise: 3,
+    assert_receive: 1,
+    assert_receive: 2,
+    assert_receive: 3,
+    assert_received: 1,
+    assert_received: 2,
+    doctest: 1,
+    doctest: 2,
+    refute: 1,
+    refute: 2,
+    refute_in_delta: 3,
+    refute_in_delta: 4,
+    refute_receive: 1,
+    refute_receive: 2,
+    refute_receive: 3,
+    refute_received: 1,
+    refute_received: 2,
+    setup: 1,
+    setup: 2,
+    setup_all: 1,
+    setup_all: 2,
+    test: 1,
+    test: 2,
+
+    # Mix config
+    config: 2,
+    config: 3,
+    import_config: 1
+  ]
+
   def elixir_sense_expand(%Env{} = env) do
     {doc_string, position} = strip_struct_operator(env)
 
@@ -20,7 +105,9 @@ defmodule Lexical.RemoteControl.Completion do
       []
     else
       {_formatter, opts} = Format.formatter_for_file(env.project, env.document.path)
-      locals_without_parens = Keyword.get(opts, :locals_without_parens, [])
+
+      locals_without_parens =
+        Keyword.get(opts, :locals_without_parens, []) ++ @built_in_locals_without_parens
 
       for suggestion <- ElixirSense.suggestions(doc_string, line, character),
           candidate = from_elixir_sense(suggestion, locals_without_parens),
@@ -39,25 +126,21 @@ defmodule Lexical.RemoteControl.Completion do
   defp maybe_suppress_parens(%struct{} = candidate, locals_without_parens)
        when struct in [Candidate.Function, Candidate.Macro] do
     atom_name = String.to_atom(candidate.name)
+    suppress_parens? = local_without_parens?(atom_name, candidate.arity, locals_without_parens)
 
-    suppress_parens? =
-      locals_without_parens
-      |> Keyword.take([atom_name])
-      |> Keyword.values()
-      |> Enum.any?(&suppress_parens?(&1, candidate.arity))
-
-    if suppress_parens? do
-      %{candidate | parens: false}
-    else
-      candidate
-    end
+    %{candidate | parens: not suppress_parens?}
   end
 
   defp maybe_suppress_parens(candidate, _), do: candidate
 
-  defp suppress_parens?(:*, _), do: true
-  defp suppress_parens?(arity, arity), do: true
-  defp suppress_parens?(_, _), do: false
+  defp local_without_parens?(fun, arity, locals_without_parens) do
+    arity > 0 and
+      Enum.any?(locals_without_parens, fn
+        {^fun, :*} -> true
+        {^fun, ^arity} -> true
+        _ -> false
+      end)
+  end
 
   def struct_fields(%Analysis{} = analysis, %Position{} = position) do
     container_struct_module =
