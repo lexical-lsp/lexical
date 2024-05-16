097c74e0b22c36db833b48a0eca00b0ed6abbea4
Zach Allaun <zach.allaun@gmail.com>
2024-05-16 11:12:12 -0400

    Merge built-in locals without parens when fetching formatter opts

--- apps/remote_control/lib/lexical/remote_control/code_mod/format.ex
+++ apps/remote_control/lib/lexical/remote_control/code_mod/format.ex
@@ -8,6 +8,91 @@ defmodule Lexical.RemoteControl.CodeMod.Format do
 
   require Logger
 
+  @built_in_locals_without_parens [
+    # Special forms
+    alias: 1,
+    alias: 2,
+    case: 2,
+    cond: 1,
+    for: :*,
+    import: 1,
+    import: 2,
+    quote: 1,
+    quote: 2,
+    receive: 1,
+    require: 1,
+    require: 2,
+    try: 1,
+    with: :*,
+
+    # Kernel
+    def: 1,
+    def: 2,
+    defp: 1,
+    defp: 2,
+    defguard: 1,
+    defguardp: 1,
+    defmacro: 1,
+    defmacro: 2,
+    defmacrop: 1,
+    defmacrop: 2,
+    defmodule: 2,
+    defdelegate: 2,
+    defexception: 1,
+    defoverridable: 1,
+    defstruct: 1,
+    destructure: 2,
+    raise: 1,
+    raise: 2,
+    reraise: 2,
+    reraise: 3,
+    if: 2,
+    unless: 2,
+    use: 1,
+    use: 2,
+
+    # Stdlib,
+    defrecord: 2,
+    defrecord: 3,
+    defrecordp: 2,
+    defrecordp: 3,
+
+    # Testing
+    assert: 1,
+    assert: 2,
+    assert_in_delta: 3,
+    assert_in_delta: 4,
+    assert_raise: 2,
+    assert_raise: 3,
+    assert_receive: 1,
+    assert_receive: 2,
+    assert_receive: 3,
+    assert_received: 1,
+    assert_received: 2,
+    doctest: 1,
+    doctest: 2,
+    refute: 1,
+    refute: 2,
+    refute_in_delta: 3,
+    refute_in_delta: 4,
+    refute_receive: 1,
+    refute_receive: 2,
+    refute_receive: 3,
+    refute_received: 1,
+    refute_received: 2,
+    setup: 1,
+    setup: 2,
+    setup_all: 1,
+    setup_all: 2,
+    test: 1,
+    test: 2,
+
+    # Mix config
+    config: 2,
+    config: 3,
+    import_config: 1
+  ]
+
   @type formatter_function :: (String.t() -> any) | nil
 
   @spec edits(Document.t()) :: {:ok, Changes.t()} | {:error, any}
@@ -77,32 +162,43 @@ defmodule Lexical.RemoteControl.CodeMod.Format do
   def formatter_for_file(%Project{} = project, file_path) do
     fetch_formatter = fn _ -> Mix.Tasks.Format.formatter_for_file(file_path) end
 
-    if RemoteControl.project_node?() do
-      case RemoteControl.Mix.in_project(project, fetch_formatter) do
-        {:ok, result} ->
-          result
+    {formatter_function, opts} =
+      if RemoteControl.project_node?() do
+        case RemoteControl.Mix.in_project(project, fetch_formatter) do
+          {:ok, result} ->
+            result
 
-        _error ->
-          formatter_opts =
-            case find_formatter_exs(project, file_path) do
-              {:ok, opts} ->
-                opts
+          _error ->
+            formatter_opts =
+              case find_formatter_exs(project, file_path) do
+                {:ok, opts} ->
+                  opts
 
-              :error ->
-                Logger.warning("Could not find formatter options for file #{file_path}")
-                []
+                :error ->
+                  Logger.warning("Could not find formatter options for file #{file_path}")
+                  []
+              end
+
+            formatter = fn source ->
+              formatted_source = Code.format_string!(source, formatter_opts)
+              IO.iodata_to_binary([formatted_source, ?\n])
             end
 
-          formatter = fn source ->
-            formatted_source = Code.format_string!(source, formatter_opts)
-            IO.iodata_to_binary([formatted_source, ?\n])
-          end
-
-          {formatter, formatter_opts}
+            {formatter, formatter_opts}
+        end
+      else
+        fetch_formatter.(nil)
       end
-    else
-      fetch_formatter.(nil)
-    end
+
+    opts =
+      Keyword.update(
+        opts,
+        :locals_without_parens,
+        @built_in_locals_without_parens,
+        &(@built_in_locals_without_parens ++ &1)
+      )
+
+    {formatter_function, opts}
   end
 
   defp find_formatter_exs(%Project{} = project, file_path) do
--- apps/remote_control/lib/lexical/remote_control/completion.ex
+++ apps/remote_control/lib/lexical/remote_control/completion.ex
@@ -9,91 +9,6 @@ defmodule Lexical.RemoteControl.Completion do
 
   import Document.Line
 
-  @built_in_locals_without_parens [
-    # Special forms
-    alias: 1,
-    alias: 2,
-    case: 2,
-    cond: 1,
-    for: :*,
-    import: 1,
-    import: 2,
-    quote: 1,
-    quote: 2,
-    receive: 1,
-    require: 1,
-    require: 2,
-    try: 1,
-    with: :*,
-
-    # Kernel
-    def: 1,
-    def: 2,
-    defp: 1,
-    defp: 2,
-    defguard: 1,
-    defguardp: 1,
-    defmacro: 1,
-    defmacro: 2,
-    defmacrop: 1,
-    defmacrop: 2,
-    defmodule: 2,
-    defdelegate: 2,
-    defexception: 1,
-    defoverridable: 1,
-    defstruct: 1,
-    destructure: 2,
-    raise: 1,
-    raise: 2,
-    reraise: 2,
-    reraise: 3,
-    if: 2,
-    unless: 2,
-    use: 1,
-    use: 2,
-
-    # Stdlib,
-    defrecord: 2,
-    defrecord: 3,
-    defrecordp: 2,
-    defrecordp: 3,
-
-    # Testing
-    assert: 1,
-    assert: 2,
-    assert_in_delta: 3,
-    assert_in_delta: 4,
-    assert_raise: 2,
-    assert_raise: 3,
-    assert_receive: 1,
-    assert_receive: 2,
-    assert_receive: 3,
-    assert_received: 1,
-    assert_received: 2,
-    doctest: 1,
-    doctest: 2,
-    refute: 1,
-    refute: 2,
-    refute_in_delta: 3,
-    refute_in_delta: 4,
-    refute_receive: 1,
-    refute_receive: 2,
-    refute_receive: 3,
-    refute_received: 1,
-    refute_received: 2,
-    setup: 1,
-    setup: 2,
-    setup_all: 1,
-    setup_all: 2,
-    test: 1,
-    test: 2,
-
-    # Mix config
-    config: 2,
-    config: 3,
-    import_config: 1
-  ]
-
   def elixir_sense_expand(%Env{} = env) do
     {doc_string, position} = strip_struct_operator(env)
 
@@ -105,9 +20,7 @@ defmodule Lexical.RemoteControl.Completion do
       []
     else
       {_formatter, opts} = Format.formatter_for_file(env.project, env.document.path)
-
-      locals_without_parens =
-        Keyword.get(opts, :locals_without_parens, []) ++ @built_in_locals_without_parens
+      locals_without_parens = Keyword.fetch!(opts, :locals_without_parens)
 
       for suggestion <- ElixirSense.suggestions(doc_string, line, character),
           candidate = from_elixir_sense(suggestion, locals_without_parens),
