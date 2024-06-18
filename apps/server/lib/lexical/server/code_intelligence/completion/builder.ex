92f25e9628bfa5d40bf7791b2154e09c2db0ed73
Zach Allaun <zach.allaun@gmail.com>
2024-06-18 10:42:39 -0400

    Co-locate and simplify completion abstractions in server

--- apps/server/lib/lexical/server/code_intelligence/completion/builder.ex
+++ apps/server/lib/lexical/server/code_intelligence/completion/builder.ex
@@ -1,6 +1,6 @@
 defmodule Lexical.Server.CodeIntelligence.Completion.Builder do
   @moduledoc """
-  Default completion builder.
+  Completion builder.
 
   For broader compatibility and control, this builder always creates text
   edits, as opposed to simple text insertions. This allows the replacement
@@ -12,30 +12,93 @@ defmodule Lexical.Server.CodeIntelligence.Completion.Builder do
 
   alias Future.Code, as: Code
   alias Lexical.Ast.Env
-  alias Lexical.Completion.Builder
-  alias Lexical.Completion.SortScope
   alias Lexical.Document
   alias Lexical.Document.Edit
   alias Lexical.Document.Position
   alias Lexical.Document.Range
   alias Lexical.Protocol.Types.Completion
   alias Lexical.Protocol.Types.Markup.Content
+  alias Lexical.Server.CodeIntelligence.Completion.SortScope
 
-  @behaviour Builder
+  @type insert_text_format :: :plain_text | :snippet
 
-  @impl Builder
+  @type completion_item_kind ::
+          :text
+          | :method
+          | :function
+          | :constructor
+          | :field
+          | :variable
+          | :class
+          | :interface
+          | :module
+          | :property
+          | :unit
+          | :value
+          | :enum
+          | :keyword
+          | :snippet
+          | :color
+          | :file
+          | :reference
+          | :folder
+          | :enum_member
+          | :constant
+          | :struct
+          | :event
+          | :operator
+          | :type_parameter
+
+  @type completion_item_tag :: :deprecated
+
+  @type item_opt ::
+          {:deprecated, boolean}
+          | {:detail, String.t()}
+          | {:documentation, String.t()}
+          | {:filter_text, String.t()}
+          | {:insert_text, String.t()}
+          | {:kind, completion_item_kind}
+          | {:label, String.t()}
+          | {:preselect, boolean()}
+          | {:sort_text, String.t()}
+          | {:tags, [completion_item_tag]}
+
+  @type item_opts :: [item_opt]
+
+  @type maybe_string :: String.t() | nil
+
+  @opaque translated_item :: %{
+            __struct__: module(),
+            detail: maybe_string(),
+            documentation: maybe_string(),
+            filter_text: maybe_string(),
+            insert_text: String.t(),
+            kind: completion_item_kind(),
+            label: String.t(),
+            preselect: boolean | nil,
+            sort_text: maybe_string(),
+            tags: [completion_item_tag] | nil
+          }
+
+  @type t :: module()
+
+  @type result :: translated_item | :skip
+
+  @type line_range :: {start_character :: pos_integer, end_character :: pos_integer}
+
+  @spec snippet(Env.t(), String.t(), item_opts) :: translated_item()
   def snippet(%Env{} = env, text, options \\ []) do
     range = prefix_range(env)
     text_edit_snippet(env, text, range, options)
   end
 
-  @impl Builder
+  @spec plain_text(Env.t(), String.t(), item_opts) :: translated_item()
   def plain_text(%Env{} = env, text, options \\ []) do
     range = prefix_range(env)
     text_edit(env, text, range, options)
   end
 
-  @impl Builder
+  @spec text_edit(Env.t(), String.t(), line_range, item_opts) :: translated_item()
   def text_edit(%Env{} = env, text, {start_char, end_char}, options \\ []) do
     line_number = env.position.line
 
@@ -54,7 +117,8 @@ defmodule Lexical.Server.CodeIntelligence.Completion.Builder do
     |> set_sort_scope(SortScope.default())
   end
 
-  @impl Builder
+  @spec text_edit_snippet(Env.t(), String.t(), line_range, item_opts) ::
+          translated_item()
   def text_edit_snippet(%Env{} = env, text, {start_char, end_char}, options \\ []) do
     snippet = String.trim_trailing(text, "\n")
     line_number = env.position.line
@@ -75,12 +139,12 @@ defmodule Lexical.Server.CodeIntelligence.Completion.Builder do
     |> set_sort_scope(SortScope.default())
   end
 
-  @impl Builder
+  @spec fallback(any, any) :: any
   def fallback(nil, fallback), do: fallback
   def fallback("", fallback), do: fallback
   def fallback(detail, _), do: detail
 
-  @impl Builder
+  @spec set_sort_scope(translated_item, sort_scope :: String.t()) :: translated_item
   def set_sort_scope(item, default \\ SortScope.default())
 
   def set_sort_scope(%Completion.Item{} = item, sort_scope)
