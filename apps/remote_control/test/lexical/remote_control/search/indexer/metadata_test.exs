9d50aa366343d0a0c12e12771dd9a1e199ac33b2
Scott Ming <therealscottming@gmail.com>
2024-06-27 07:54:28 +0800

    Fix metadata tests and implementations
    Sourceror has already fixed the errors related to anonymous functions,
    so we no longer need to handle such functions specially.

--- apps/remote_control/test/lexical/remote_control/search/indexer/metadata_test.exs
+++ apps/remote_control/test/lexical/remote_control/search/indexer/metadata_test.exs
@@ -153,12 +153,12 @@ defmodule Lexical.RemoteControl.Search.Indexer.MetadataTest do
 
     test "are found in anonymous functions with no body" do
       code = "fn -> :ok end"
-      assert "«fn -> :ok »end" == decorate_location(code)
+      assert "«fn -> :ok end»" == decorate_location(code)
     end
 
     test "are found in single line anonymous functions" do
       code = "fn arg -> arg + 1  end"
-      assert "«fn arg -> arg + 1  »end" == decorate_location(code)
+      assert "«fn arg -> arg + 1  end»" == decorate_location(code)
     end
 
     test "are found in multiple line anonymous functions" do
@@ -167,15 +167,14 @@ defmodule Lexical.RemoteControl.Search.Indexer.MetadataTest do
           y = do_something_with(a)
           z = do_something_with(b)
           y + z
-        end
-      ]
+        end]
 
       expected = ~q[
         «fn a, b ->
           y = do_something_with(a)
           z = do_something_with(b)
           y + z
-        »end
+        end»
       ]t
 
       assert expected == decorate_location(code)
