6572a6a04a083e4857efad3dadef46200a70f5db
Zach Allaun <zach.allaun@gmail.com>
2024-07-23 09:56:57 -0700

    Fix crashes on OTP 27 by disabling ETS table compression (#796)
    * Fix crashes on OTP 27 by disabling ETS table compression
    
    * Reference erlang/otp issue in docstring
    
    * Switch to Erlang version check that works on X.X.X.X versions

--- apps/common/lib/elixir/features.ex
+++ apps/common/lib/elixir/features.ex
@@ -1,4 +1,6 @@
 defmodule Elixir.Features do
+  alias Lexical.VM.Versions
+
   def with_diagnostics? do
     function_exported?(Code, :with_diagnostics, 1)
   end
@@ -21,5 +23,21 @@ defmodule Elixir.Features do
 
   def contains_set_theoretic_types? do
     Version.match?(System.version(), ">= 1.17.0")
+  end
+
+  @doc """
+  Whether the `:compressed` ETS table option can be safely used.
+
+  A bug in Erlang/OTP 27.0.0 and 27.0.1 can cause a segfault when
+  traversing the entire table with something like `:ets.foldl/3` if the
+  `:compressed` table option is used. When this is fixed, we can change
+  the version check to `"< 27.0.0 or >= 27.X"`.
+
+  Issue to track: https://github.com/erlang/otp/issues/8682
+  """
+  def can_use_compressed_ets_table? do
+    %{erlang: erlang_version} = Versions.to_versions(Versions.current())
+
+    Version.match?(erlang_version, "< 27.0.0")
   end
 end
