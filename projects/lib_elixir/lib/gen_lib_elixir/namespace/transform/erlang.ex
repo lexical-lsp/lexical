0bae1cee60164cd49afde2f2bf0e94b694da3dc6
Zach Allaun <zach.allaun@gmail.com>
2024-07-30 12:49:42 -0400

    Remove unused functions

--- projects/lib_elixir/lib/gen_lib_elixir/namespace/transform/erlang.ex
+++ projects/lib_elixir/lib/gen_lib_elixir/namespace/transform/erlang.ex
@@ -9,25 +9,10 @@ defmodule LibElixir.Namespace.Transform.Erlang do
     end
   end
 
-  def path_to_ast(file_path) do
-    path_charlist = String.to_charlist(file_path)
-
-    with {:ok, [app]} <- :file.consult(path_charlist) do
-      ast_string = inspect(app)
-      Code.string_to_quoted(ast_string)
-    end
-  end
-
   def term_to_string(term) do
     ~c"~p.~n"
     |> :io_lib.format([term])
     |> :lists.flatten()
     |> List.to_string()
-  end
-
-  def ast_to_string(elixir_ast) do
-    elixir_ast
-    |> Code.eval_quoted()
-    |> term_to_string()
   end
 end
